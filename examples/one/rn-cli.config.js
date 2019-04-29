b7d11a217b3d4bff785e1c69f861cd3252678241
Vishal Narkhede <vishal@getstream.io>
2019-04-29 13:50:41 +0200

    build deps

---
+++ examples/one/rn-cli.config.js
@@ -0,0 +1,53 @@
+/* eslint-env node */
+
+const fs = require('fs');
+const path = require('path');
+const blacklist = require('metro/src/blacklist');
+const repoDir = path.dirname(__dirname);
+
+module.exports = {
+  getBlacklistRE() {
+    return blacklist([
+    //   new RegExp(repoDir + '/example/'),
+    //   new RegExp(repoDir + '/native-example/(.*)'),
+    //   new RegExp(repoDir + '/native-package/(.*)'),
+    //   new RegExp(repoDir + '/expo-package/node_modules/(.*)'),
+      new RegExp(repoDir + '/node_modules/(.*)'),
+    ]);
+  },
+  extraNodeModules: getNodeModulesForDirectory(path.resolve('.')),
+};
+
+function getNodeModulesForDirectory(rootPath) {
+  const nodeModulePath = path.join(rootPath, 'node_modules');
+  const folders = fs.readdirSync(nodeModulePath);
+  return folders.reduce((modules, folderName) => {
+    const folderPath = path.join(nodeModulePath, folderName);
+    if (folderName.startsWith('@')) {
+      const scopedModuleFolders = fs.readdirSync(folderPath);
+      const scopedModules = scopedModuleFolders.reduce(
+        (scopedModules, scopedFolderName) => {
+          scopedModules[
+            `${folderName}/${scopedFolderName}`
+          ] = maybeResolveSymlink(path.join(folderPath, scopedFolderName));
+          return scopedModules;
+        },
+        {},
+      );
+      return Object.assign({}, modules, scopedModules);
+    }
+    modules[folderName] = maybeResolveSymlink(folderPath);
+    return modules;
+  }, {});
+}
+
+function maybeResolveSymlink(maybeSymlinkPath) {
+  if (fs.lstatSync(maybeSymlinkPath).isSymbolicLink()) {
+    const resolved = path.resolve(
+      path.dirname(maybeSymlinkPath),
+      fs.readlinkSync(maybeSymlinkPath),
+    );
+    return resolved;
+  }
+  return maybeSymlinkPath;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export { default as FooComponent } from './components/Foo';
+export { Chat } from './components/Chat';
+export { Channel } from './components/Channel';
+export { MessageList } from './components/MessageList';
+export { MessageInput } from './components/MessageInput';
+export { ChannelList } from './components/ChannelList';
+export { Thread } from './components/Thread' ;
+export { ChannelPreviewMessenger } from './components/ChannelPreviewMessenger';
