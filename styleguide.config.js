a76584de3b573d86df8bc9b9eec4dc2849c4fb5f
Jelte Fennema <github-tech@jeltef.nl>
2019-05-27 12:04:50 +0200

    Move styleguide Slot renderer to repo, so it can be customized

--- styleguide.config.js
+++ styleguide.config.js
@@ -30,6 +30,7 @@ module.exports = {
       __dirname,
       'src/styleguideComponents/PathlineRenderer.js',
     ),
+    Slot: path.join(__dirname, 'src/styleguideComponents/Slot.js'),
   },
 
   sections: [
