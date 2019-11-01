c0e0bd8953d279c3f50890e42f196f4c28ecdabc
Pranav Dakshinamurthy <pranav.dakshinamurthy@verizonwireless.com>
2019-11-01 00:58:17 -0400

    Add support for Custom Icons in SendButton
    - This should support both Editing 
    Icon and New message Icon.

--- src/components/SendButton.js
+++ src/components/SendButton.js
@@ -31,15 +31,53 @@ export const SendButton = themed(
       editing: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
       /** Function that sends message */
       sendMessage: PropTypes.func.isRequired,
+      /** Custom Icon for sending New message **/
+      newMessageIcon: PropTypes.oneOfType([
+        PropTypes.shape({
+          uri: PropTypes.string,
+          headers: PropTypes.objectOf(PropTypes.string),
+        }),
+        PropTypes.number,
+        PropTypes.arrayOf(
+          PropTypes.shape({
+            uri: PropTypes.string,
+            width: PropTypes.number,
+            height: PropTypes.number,
+            headers: PropTypes.objectOf(PropTypes.string),
+          }),
+        ),
+      ]),
+      /** Custom Icon for editing message **/
+      editMessageIcon: PropTypes.oneOfType([
+        PropTypes.shape({
+          uri: PropTypes.string,
+          headers: PropTypes.objectOf(PropTypes.string),
+        }),
+        PropTypes.number,
+        PropTypes.arrayOf(
+          PropTypes.shape({
+            uri: PropTypes.string,
+            width: PropTypes.number,
+            height: PropTypes.number,
+            headers: PropTypes.objectOf(PropTypes.string),
+          }),
+        ),
+      ]),
     };
     render() {
-      const { sendMessage, editing, title } = this.props;
+      const {
+        sendMessage,
+        editing,
+        title,
+        editMessageIcon,
+        newMessageIcon,
+      } = this.props;
       return (
         <Container title={title} onPress={sendMessage}>
           {editing ? (
-            <SendButtonIcon source={iconEdit} />
+            <SendButtonIcon source={editMessageIcon || iconEdit} />
           ) : (
-            <SendButtonIcon source={iconSendNewMessage} />
+            <SendButtonIcon source={newMessageIcon || iconSendNewMessage} />
           )}
         </Container>
       );
