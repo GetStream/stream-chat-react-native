d8aeb19a62a10eff2b23d0b35d0abea742894774
Vir Desai <virdesai@virs-mbp.lan>
2020-04-08 16:42:55 -0400

    removing import for styled-component

--- src/components/IconSquare.js
+++ src/components/IconSquare.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { TouchableOpacity, Image } from 'react-native';
+import { Image } from 'react-native';
 import PropTypes from 'prop-types';
 import styled from '@stream-io/styled-components';
 
-const StyledTouchableOpacity = styled(TouchableOpacity)`
+const StyledTouchableOpacity = styled.TouchableOpacity`
   background-color: rgba(0, 0, 0, 0.05);
   border-radius: 5px;
   padding: 5px;
