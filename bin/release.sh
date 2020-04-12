b7c85c4d4db8b087e91ac9719544f5eb4b2a8a14
vishtree <vishal@getstream.io>
2020-04-12 16:27:56 +0200

    Updating release script

--- bin/release.sh
+++ bin/release.sh
@@ -12,23 +12,34 @@ cd native-package
 npm version --no-git-tag-version "$1"
 sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$1"'"|g' -i.bak package.json
 rm package.json.bak
+
+cd ../native-web-package
+npm version --no-git-tag-version "$1"
+sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$1"'"|g' -i.bak package.json
+rm package.json.bak
+
 cd ../expo-package
 npm version --no-git-tag-version "$1"
 sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$1"'"|g' -i.bak package.json
 rm package.json.bak
 cd ..
-git add {expo,native}-package/package.json
+git add {expo,native,native-web}-package/package.json
 #yarn docs
 #git add docs
 git add expo-package/yarn.lock
 git add native-package/yarn.lock
+git add native-web-package/yarn.lock
+
 npm version "$1" --force
 
-
 npm publish
 
-cd native-package
+cd native-web-package
 npm publish
+
+cd ../native-package
+npm publish
+
 
 cd ../expo-package
 npm publish
