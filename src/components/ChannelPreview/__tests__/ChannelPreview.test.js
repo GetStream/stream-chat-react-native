e7d277634f2c5493a36cc1f4cec085bdf6c9a499
Dan Carbonell <43552931+DanC5@users.noreply.github.com>
2020-07-28 10:10:32 -0600

    Update src/components/ChannelPreview/__tests__/ChannelPreview.test.js
    Co-authored-by: Vir Desai <virdesai@users.noreply.github.com>

--- src/components/ChannelPreview/__tests__/ChannelPreview.test.js
+++ src/components/ChannelPreview/__tests__/ChannelPreview.test.js
@@ -29,7 +29,7 @@ const ChannelPreviewUIComponent = (props) => (
     <Text testID="channel-id">{props.channel.id}</Text>
     <Text testID="unread-count">{props.unread}</Text>
     <Text testID="last-event-message">
-      {props.lastMessage && props.lastMessage.text}
+      {props?.lastMessage?.text}
     </Text>
     <Text testID='latest-message'>
       {props.latestMessage && props.latestMessage.text}
