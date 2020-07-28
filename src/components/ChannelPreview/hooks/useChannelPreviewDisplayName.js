078b5fa3c7a648961f85f2f16a62ab7ae0e54dfb
Dan Carbonell <43552931+DanC5@users.noreply.github.com>
2020-07-28 09:47:41 -0600

    Update src/components/ChannelPreview/hooks/useChannelPreviewDisplayName.js
    Co-authored-by: Vir Desai <virdesai@users.noreply.github.com>

--- src/components/ChannelPreview/hooks/useChannelPreviewDisplayName.js
+++ src/components/ChannelPreview/hooks/useChannelPreviewDisplayName.js
@@ -11,7 +11,7 @@ export const useChannelPreviewDisplayName = (channel) => {
       return;
     }
 
-    const members = channel.state ? Object.values(channel.state.members) : [];
+    const members = Object.values(channel?.state?.members || {});
     const otherMembers = members.filter(
       (member) => member.user.id !== client.user.id,
     );
