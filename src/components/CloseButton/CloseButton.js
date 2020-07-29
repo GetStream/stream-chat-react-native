a40b9217ea5f5fee8dbfc831c90650ad62af2ee5
Vir Desai <virdesai@virs-mbp.lan>
2020-07-29 11:00:18 -0400

    CRNS-147: converting CloseButton component from class to functional component and adding hooks

--- src/components/CloseButton/CloseButton.js
+++ src/components/CloseButton/CloseButton.js
@@ -3,33 +3,27 @@ import { Image } from 'react-native';
 
 import styled from '@stream-io/styled-components';
 
-import { themed } from '../../styles/theme';
 import closeRound from '../../images/icons/close-round.png';
 
+import { themed } from '../../styles/theme';
+
 const Container = styled.View`
-  width: 30;
-  height: 30;
-  border-radius: 3;
   align-items: center;
-  justify-content: center;
   background-color: white;
   border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  height: 30px;
+  justify-content: center;
+  width: 30px;
   ${({ theme }) => theme.closeButton.container.css}
 `;
 
-const CloseButton = themed(
-  class CloseButton extends React.PureComponent {
-    static themePath = 'closeButton';
-    render() {
-      return (
-        <Container>
-          <Image source={closeRound} />
-        </Container>
-      );
-    }
-  },
+const CloseButton = () => (
+  <Container>
+    <Image source={closeRound} testID='close-button' />
+  </Container>
 );
 
-CloseButton.propTypes = {};
+CloseButton.themePath = 'closeButton';
 
-export default CloseButton;
+export default themed(CloseButton);
