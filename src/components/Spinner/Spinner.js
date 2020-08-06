4d0c085f170f270a4bc5819baba17940b4132c10
Dan Carbonell <dan@getstream.io>
2020-08-06 14:36:47 -0600

    convert Spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,62 +1,58 @@
-import React from 'react';
-import { View, Animated, Easing } from 'react-native';
+import React, { useEffect } from 'react';
+import { Animated, Easing, View } from 'react-native';
 import styled from '@stream-io/styled-components';
+
 import { themed } from '../../styles/theme';
 
 const AnimatedView = Animated.createAnimatedComponent(View);
 
 export const Circle = styled(AnimatedView)`
-  height: 30px;
-  width: 30px;
-  margin: 5px;
-  border-width: 2px;
-  border-radius: 30px;
-  justify-content: center;
   border-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 30px;
   border-right-color: transparent;
+  border-width: 2px;
+  height: 30px;
+  justify-content: center;
+  margin: 5px;
+  width: 30px;
   ${({ theme }) => theme.spinner.css}
 `;
+
 /**
  * @example ../docs/Spinner.md
- * @extends PureComponent
  */
-class Spinner extends React.PureComponent {
-  static themePath = 'spinner';
-  state = {
-    rotateValue: new Animated.Value(0),
-  };
+const Spinner = () => {
+  const rotateValue = new Animated.Value(0);
 
-  componentDidMount() {
-    this._start();
-  }
-
-  _start = () => {
+  const startSpinning = () => {
     Animated.loop(
-      Animated.timing(this.state.rotateValue, {
-        toValue: 1,
+      Animated.timing(rotateValue, {
         duration: 800,
         easing: Easing.linear,
+        toValue: 1,
         useNativeDriver: true,
       }),
     ).start();
   };
 
-  render() {
-    return (
-      <Circle
-        style={{
-          transform: [
-            {
-              rotate: this.state.rotateValue.interpolate({
-                inputRange: [0, 1],
-                outputRange: ['0deg', '360deg'],
-              }),
-            },
-          ],
-        }}
-      />
-    );
-  }
-}
+  useEffect(() => startSpinning());
+
+  return (
+    <Circle
+      style={{
+        transform: [
+          {
+            rotate: rotateValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: ['0deg', '360deg'],
+            }),
+          },
+        ],
+      }}
+    />
+  );
+};
+
+Spinner.themePath = 'spinner';
 
 export default themed(Spinner);
