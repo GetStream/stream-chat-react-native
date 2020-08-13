839bf8bea7f83766377604786c6bbe2e9c6e4065
nhannah <ne.hannah@gmail.com>
2020-08-13 14:39:49 -0600

    fix iconSquare propTypes

--- src/components/IconSquare.js
+++ src/components/IconSquare.js
@@ -33,6 +33,20 @@ export const IconSquare = ({ icon, onPress }) => {
 };
 
 IconSquare.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  icon: PropTypes.oneOfType([
+    PropTypes.shape({
+      uri: PropTypes.string,
+      headers: PropTypes.objectOf(PropTypes.string),
+    }),
+    PropTypes.number,
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        uri: PropTypes.string,
+        width: PropTypes.number,
+        height: PropTypes.number,
+        headers: PropTypes.objectOf(PropTypes.string),
+      }),
+    ),
+  ]),
   onPress: PropTypes.func,
 };
