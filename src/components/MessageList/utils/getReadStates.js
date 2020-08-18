10262e361753ba899ed54175fb16d5f031674090
nhannah <ne.hannah@gmail.com>
2020-08-18 11:48:36 -0600

    update getReadStates

--- src/components/MessageList/utils/getReadStates.js
+++ src/components/MessageList/utils/getReadStates.js
@@ -1,25 +1,45 @@
 export const getReadStates = (messages, read) => {
-  // create object with empty array for each message id
   const readData = {};
 
   for (const message of messages) {
-    // Filter out date seperators
-    if (!message || !message.id) {
+    /**
+     * Filter out date separators
+     */
+    if (!message?.id) {
       continue;
     }
+
+    /**
+     * Create empty array for each message id
+     */
     readData[message.id] = [];
   }
 
+  /**
+   * Channel read state is stored by user
+   */
   for (const readState of Object.values(read)) {
+    /**
+     * If no last read break
+     */
     if (readState.last_read == null) {
       break;
     }
+
+    /**
+     * Find the last message before the last_read
+     */
     let userLastReadMsgId;
     for (const msg of messages) {
       if (msg.updated_at < readState.last_read) {
         userLastReadMsgId = msg.id;
       }
     }
+
+    /**
+     * If there there is a last read message add the user
+     * to the array of last reads for that message
+     */
     if (userLastReadMsgId) {
       readData[userLastReadMsgId] = [
         ...readData[userLastReadMsgId],
