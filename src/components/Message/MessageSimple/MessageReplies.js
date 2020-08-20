0e67a8bd47f51f23ec80da642dd14f1c18c7b754
nhannah <ne.hannah@gmail.com>
2020-08-20 11:10:15 -0600

    Message Replies to FC

--- src/components/Message/MessageSimple/MessageReplies.js
+++ src/components/Message/MessageSimple/MessageReplies.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from '@stream-io/styled-components';
 import PropTypes from 'prop-types';
 
+import { TranslationContext } from '../../../context';
 import iconPath from '../../../images/icons/icon_path.png';
-import { withTranslationContext } from '../../../context';
 
 const Container = styled.TouchableOpacity`
   padding: 5px;
   flex-direction: row;
   align-items: center;
   ${({ theme }) => theme.message.replies.container.css}
+`;
+
+const MessageRepliesImage = styled.Image`
+  transform: ${({ alignment }) =>
+    alignment === 'left' ? 'rotateY(0deg)' : 'rotateY(180deg)'};
+  ${({ theme }) => theme.message.replies.image.css}
 `;
 
 const MessageRepliesText = styled.Text`
@@ -19,47 +25,44 @@ const MessageRepliesText = styled.Text`
   ${({ theme }) => theme.message.replies.messageRepliesText.css}
 `;
 
-const MessageRepliesImage = styled.Image`
-  transform: ${({ alignment }) =>
-    alignment === 'left' ? 'rotateY(0deg)' : 'rotateY(180deg)'};
-  ${({ theme }) => theme.message.replies.image.css}
-`;
-
-const MessageReplies = ({
-  message,
-  isThreadList,
-  openThread,
-  alignment,
-  t,
-}) => {
+const MessageReplies = ({ alignment, isThreadList, message, openThread }) => {
+  const { t } = useContext(TranslationContext);
   if (isThreadList || !message.reply_count) return null;
 
   return (
-    <Container onPress={openThread}>
-      {alignment === 'left' ? (
-        <MessageRepliesImage source={iconPath} alignment={alignment} />
-      ) : null}
+    <Container onPress={openThread} testID='message-replies'>
+      {alignment === 'left' && (
+        <MessageRepliesImage
+          alignment={alignment}
+          source={iconPath}
+          testID='message-replies-left'
+        />
+      )}
       <MessageRepliesText>
         {message.reply_count === 1
           ? t('1 reply')
           : t('{{ replyCount }} replies', { replyCount: message.reply_count })}
       </MessageRepliesText>
-      {alignment === 'right' ? (
-        <MessageRepliesImage source={iconPath} alignment={alignment} />
-      ) : null}
+      {alignment === 'right' && (
+        <MessageRepliesImage
+          alignment={alignment}
+          source={iconPath}
+          testID='message-replies-right'
+        />
+      )}
     </Container>
   );
 };
 
 MessageReplies.propTypes = {
-  /** Current [message object](https://getstream.io/chat/docs/#message_format) */
-  message: PropTypes.object,
-  /** Boolean if current message is part of thread */
-  isThreadList: PropTypes.bool,
-  /** @see See [Channel Context](https://getstream.github.io/stream-chat-react-native/#channelcontext) */
-  openThread: PropTypes.func,
   /** right | left */
   alignment: PropTypes.oneOf(['right', 'left']),
+  /** Boolean if current message is part of thread */
+  isThreadList: PropTypes.bool,
+  /** Current [message object](https://getstream.io/chat/docs/#message_format) */
+  message: PropTypes.object,
+  /** @see See [Channel Context](https://getstream.github.io/stream-chat-react-native/#channelcontext) */
+  openThread: PropTypes.func,
 };
 
-export default withTranslationContext(MessageReplies);
+export default MessageReplies;
