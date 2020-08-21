4b0085b825c7e019308d322cb9405a429304c7c0
nhannah <ne.hannah@gmail.com>
2020-08-21 10:24:13 -0600

    move constructTypingString to util

--- src/components/MessageList/utils/index.js
+++ src/components/MessageList/utils/index.js
@@ -1,3 +1,4 @@
+export { constructTypingString } from './constructTypingString';
 export { getGroupStyles } from './getGroupStyles';
 export { getLastReceivedId } from './getLastReceivedId';
 export { getReadStates } from './getReadStates';
