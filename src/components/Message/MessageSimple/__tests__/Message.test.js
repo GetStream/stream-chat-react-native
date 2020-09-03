266667385f42927e53fa2d9a79f698c2f1b86c4d
Dan Carbonell <dan@getstream.io>
2020-09-03 15:47:52 -0600

    add custom action sheet test

--- src/components/Message/MessageSimple/__tests__/Message.test.js
+++ src/components/Message/MessageSimple/__tests__/Message.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 import {
   cleanup,
   fireEvent,
@@ -332,6 +333,31 @@ describe('Message', () => {
 
     await waitFor(() => {
       expect(onLongPress).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('renders a custom action sheet when the `ActionSheet` prop exists', async () => {
+    const user = generateUser();
+    const message = generateMessage({ user });
+    // eslint-disable-next-line react/display-name
+    const ActionSheet = React.forwardRef((props, ref) => (
+      <View {...props} ref={ref} testID='action-sheet-prop' />
+    ));
+
+    const { getByTestId, queryAllByTestId } = renderMessage({
+      ActionSheet,
+      message,
+    });
+
+    await waitFor(() => {
+      expect(getByTestId('message-wrapper')).toBeTruthy();
+      expect(queryAllByTestId('action-sheet-prop')).toHaveLength(0);
+    });
+
+    fireEvent(getByTestId('message-content-wrapper'), 'longPress');
+
+    await waitFor(() => {
+      expect(getByTestId('action-sheet-prop')).toBeTruthy();
     });
   });
 });
