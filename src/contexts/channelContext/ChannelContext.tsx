bc4a21a6d2df32db8c097ed550d5c078abf818d3
nhannah <ne.hannah@gmail.com>
2020-09-17 13:25:36 -0600

    add Channel Context

---
+++ src/contexts/channelContext/ChannelContext.tsx
@@ -0,0 +1,105 @@
+import React, { PropsWithChildren, useContext } from 'react';
+import type {
+  Channel,
+  ChannelState,
+  Event,
+  LiteralStringForUnion,
+  UnknownType,
+} from 'stream-chat';
+
+import { getDisplayName } from '../utils/getDisplayName';
+
+export type ChannelContextValue<
+  At extends UnknownType = UnknownType,
+  Ch extends UnknownType = UnknownType,
+  Co extends string = LiteralStringForUnion,
+  Ev extends UnknownType = UnknownType,
+  Me extends UnknownType = UnknownType,
+  Re extends UnknownType = UnknownType,
+  Us extends UnknownType = UnknownType
+> = {
+  EmptyStateIndicator: React.ComponentType<{ listType?: string }>;
+  error: boolean;
+  eventHistory: { [key: string]: Event<At, Ch, Co, Ev, Me, Re, Us>[] };
+  loading: boolean;
+  markRead: () => void;
+  members: ChannelState<At, Ch, Co, Ev, Me, Re, Us>['members'];
+  read: ChannelState<At, Ch, Co, Ev, Me, Re, Us>['read'];
+  setLastRead: React.Dispatch<React.SetStateAction<Date | undefined>>;
+  typing: ChannelState<At, Ch, Co, Ev, Me, Re, Us>['typing'];
+  watchers: ChannelState<At, Ch, Co, Ev, Me, Re, Us>['watchers'];
+  channel?: Channel<At, Ch, Co, Ev, Me, Re, Us>;
+  disabled?: boolean;
+  lastRead?: Date;
+  watcherCount?: ChannelState<At, Ch, Co, Ev, Me, Re, Us>['watcher_count'];
+};
+
+export const ChannelContext = React.createContext({} as ChannelContextValue);
+
+export const ChannelProvider = <
+  At extends UnknownType = UnknownType,
+  Ch extends UnknownType = UnknownType,
+  Co extends string = LiteralStringForUnion,
+  Ev extends UnknownType = UnknownType,
+  Me extends UnknownType = UnknownType,
+  Re extends UnknownType = UnknownType,
+  Us extends UnknownType = UnknownType
+>({
+  children,
+  value,
+}: PropsWithChildren<{
+  value: ChannelContextValue<At, Ch, Co, Ev, Me, Re, Us>;
+}>) => (
+  <ChannelContext.Provider value={(value as unknown) as ChannelContextValue}>
+    {children}
+  </ChannelContext.Provider>
+);
+
+export const useChannelContext = <
+  At extends UnknownType = UnknownType,
+  Ch extends UnknownType = UnknownType,
+  Co extends string = LiteralStringForUnion,
+  Ev extends UnknownType = UnknownType,
+  Me extends UnknownType = UnknownType,
+  Re extends UnknownType = UnknownType,
+  Us extends UnknownType = UnknownType
+>() =>
+  (useContext(ChannelContext) as unknown) as ChannelContextValue<
+    At,
+    Ch,
+    Co,
+    Ev,
+    Me,
+    Re,
+    Us
+  >;
+
+// /**
+//  * Typescript currently does not support partial inference so if ChatContext
+//  * typing is desired while using the HOC withChannelContext the Props for the
+//  * wrapped component must be provided as the first generic.
+//  */
+export const withChannelContext = <
+  P extends Record<string, unknown>,
+  At extends UnknownType = UnknownType,
+  Ch extends UnknownType = UnknownType,
+  Co extends string = LiteralStringForUnion,
+  Ev extends UnknownType = UnknownType,
+  Me extends UnknownType = UnknownType,
+  Re extends UnknownType = UnknownType,
+  Us extends UnknownType = UnknownType
+>(
+  Component: React.ComponentType<P>,
+): React.FC<Omit<P, keyof ChannelContextValue<At, Ch, Co, Ev, Me, Re, Us>>> => {
+  const WithChannelContextComponent = (
+    props: Omit<P, keyof ChannelContextValue<At, Ch, Co, Ev, Me, Re, Us>>,
+  ) => {
+    const channelContext = useChannelContext<At, Ch, Co, Ev, Me, Re, Us>();
+
+    return <Component {...(props as P)} {...channelContext} />;
+  };
+  WithChannelContextComponent.displayName = `WithChannelContext${getDisplayName(
+    Component,
+  )}`;
+  return WithChannelContextComponent;
+};
