2748dd39f5242cc5b38d8ec58e4a719fccdb5034
Dan Carbonell <43552931+DanC5@users.noreply.github.com>
2020-09-22 11:39:04 -0600

    Update src/components/Attachment/Attachment.tsx
    Co-authored-by: Vir Desai <virdesai@users.noreply.github.com>

--- src/components/Attachment/Attachment.tsx
+++ src/components/Attachment/Attachment.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+
 import type { Attachment as AttachmentType, UnknownType } from 'stream-chat';
 
 import DefaultAttachmentActions, {
