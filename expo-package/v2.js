3e7ceebc757ad45e174225a491f94f8a6e140043
Vir Desai <vir.desai.nc@gmail.com>
2020-10-14 17:17:26 -0400

    now that we support only expo >=39 removing extra stuff in expo-package

--- expo-package/v2.js
+++ expo-package/v2.js
@@ -1,31 +1,8 @@
-/**
- * In Expo 34, all the modules such as image picker, document picker, permissions etc etc have been moved to their own packages.
- * And they have been removed from Expo package. But these packages don't work with Expo 32 sdk (because of linking issue).
- */
 import { registerNativeHandlers } from 'stream-chat-react-native-core';
 import NetInfo from '@react-native-community/netinfo';
-import { Constants } from 'react-native-unimodules';
-import * as Expo from 'expo';
-
-let ImagePicker;
-let DocumentPicker;
-let Permissions;
-let manifest = {};
-
-manifest = Constants.manifest;
-if (!manifest) {
-  manifest = Expo.Constants.manifest;
-}
-
-if (manifest.sdkVersion.split('.')[0] >= 33) {
-  ImagePicker = require('expo-image-picker');
-  Permissions = require('expo-permissions');
-  DocumentPicker = require('expo-document-picker');
-} else {
-  ImagePicker = Expo.ImagePicker;
-  Permissions = Expo.Permissions;
-  DocumentPicker = Expo.DocumentPicker;
-}
+import * as DocumentPicker from 'expo-document-picker';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 registerNativeHandlers({
   NetInfo: {
