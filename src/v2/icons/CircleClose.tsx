59c09e4a8288ba1a10dd642795bf38395cfd3c96
Vir Desai <vir.desai.nc@gmail.com>
2020-10-19 22:45:56 -0400

    fix export name

--- src/v2/icons/CircleClose.tsx
+++ src/v2/icons/CircleClose.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { IconProps, RootPath, RootSvg } from './utils/base';
 
-export const CircleRight: React.FC<IconProps> = (props) => (
+export const CircleClose: React.FC<IconProps> = (props) => (
   <RootSvg {...props}>
     <RootPath
       d='M14.121 8.464a1 1 0 111.415 1.415L13.414 12l2.122 2.121a1 1 0 01-1.415 1.415l-2.12-2.122-2.122 2.122a1 1 0 11-1.414-1.415L10.585 12l-2.12-2.121a1 1 0 111.414-1.415L12 10.586l2.121-2.122z'
