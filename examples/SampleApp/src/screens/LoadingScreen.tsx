6fa53b9761eca6d11e6477f7c0a2743d88d20473
nhannah <ne.hannah@gmail.com>
2020-12-29 18:00:11 -0700

    fix merge ts errors

--- examples/SampleApp/src/screens/LoadingScreen.tsx
+++ examples/SampleApp/src/screens/LoadingScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   ActivityIndicator,
-  SafeAreaView,
   StyleSheet,
   useColorScheme,
   View,
@@ -11,34 +10,33 @@ import { useTheme } from 'stream-chat-react-native/v2';
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    height: '100%',
+    flex: 1,
     justifyContent: 'center',
-    width: '100%',
   },
 });
 
-export const LoadingScreen = () => {
+export const LoadingScreen: React.FC = () => {
   const colorScheme = useColorScheme();
   const { theme } = useTheme();
+
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor:
-          theme?.colors?.white_snow || colorScheme === 'dark'
-            ? '#070A0D'
-            : '#FCFCFC',
-      }}
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor:
+            theme?.colors?.white_snow || colorScheme === 'dark'
+              ? '#070A0D'
+              : '#FCFCFC',
+        },
+      ]}
     >
-      <View style={styles.container}>
-        <ActivityIndicator
-          color={
-            theme?.colors?.black || colorScheme === 'dark'
-              ? '#FFFFFF'
-              : '#000000'
-          }
-          size='small'
-        />
-      </View>
-    </SafeAreaView>
+      <ActivityIndicator
+        color={
+          theme?.colors?.black || colorScheme === 'dark' ? '#FFFFFF' : '#000000'
+        }
+        size='small'
+      />
+    </View>
   );
 };
