260d560cf3384c1d11c8d235bc6f096fd737b08e
charles francis <charlsefrancis112@gmail.com>
2021-02-01 12:15:29 +0800

    Update App.js

--- examples/NativeMessaging/App.js
+++ examples/NativeMessaging/App.js
@@ -4,6 +4,8 @@ import {LogBox, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator, useHeaderHeight} from '@react-navigation/stack';
 import {enableScreens} from 'react-native-screens';
+import { Button, Text, StyleSheet, StatusBar, View } from "react-native";
+import Constants from "expo-constants";
 import {StreamChat} from 'stream-chat';
 import {
   Channel,
@@ -36,10 +38,82 @@ const theme = {
     `,
   },
 };
+//status bar
+
+const App = () => {
+  const styleTypes = ['default','dark-content', 'light-content'];
+  const [visibleStatusBar, setVisibleStatusBar] = useState(false);
+  const [styleStatusBar, setStyleStatusBar] = useState(styleTypes[0]);
+
+  const changeVisibilityStatusBar = () => {
+    setVisibleStatusBar(!visibleStatusBar);
+  };
+
+  const changeStyleStatusBar = () => {
+    const styleId = styleTypes.indexOf(styleStatusBar) + 1;
+
+    if(styleId === styleTypes.length){
+      return setStyleStatusBar(styleTypes[0]);
+    }
+    return setStyleStatusBar(styleTypes[styleId]);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.textStyle}>StatusBar Style: {styleStatusBar}</Text>
+        <Text style={styles.textStyle}>StatusBar Visibility: {!visibleStatusBar ? 'Visible': 'Hidden'}</Text>
+      </View>
+      <StatusBar backgroundColor="blue" barStyle={styleStatusBar} />
+      <View>
+        <StatusBar hidden={visibleStatusBar} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Toggle StatusBar" onPress={() => changeVisibilityStatusBar()} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Change StatusBar Style" onPress={() => changeStyleStatusBar()} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ECF0F1',
+    padding: 8
+  },
+  buttonContainer:{
+    padding: 10
+  },
+  textStyle:{
+    textAlign: 'center'
+  }
+});
+
+export default App
+
+render: function() {
+  return (
+    <ToolbarAndroid
+      logo={require('./app_logo.png')}
+      title="AwesomeApp"
+      actions={[{title: 'Settings', icon: require('./icon_settings.png'), show: 'always'}]}
+      onActionSelected={this.onActionSelected} />
+  )
+},
+onActionSelected: function(position) {
+  if (position === 0) { // index of 'Settings'
+    showSettings();
+  }
+}
 
 const chatClient = new StreamChat('q95x9hkbyd6p');
 const userToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9uIn0.eRVjxLvd4aqCEHY_JRa97g6k7WpHEhxL7Z4K4yTot1c';
+'jkndykrr7cepdnp5q8kyghrjjbbdgj8hrntn8n7jjwabqyh53axgj3h5h3kegp32';
 const user = {
   id: 'ron',
 };
