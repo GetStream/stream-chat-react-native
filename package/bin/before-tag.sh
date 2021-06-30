72baae08c9dac41dc84f80615d636b1192ca36b8
lucas correia <tsirlucas@gmail.com>
2021-06-30 17:57:17 -0300

    chore: prepare workflows for automated releases

---
+++ package/bin/before-tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# shellcheck disable=SC2103
+
+
+# Runs before lerna tagging in order to set correct tags for internal native-package
+# and expo-package. also sets version.json. also runs before publishing a nightly release
+
+set -eux
+
+PACKAGE_VERSION=$(npm --loglevel silent run get-version)
+
+cd native-package
+npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION"
+sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION"'"|g' -i.bak package.json
+rm package.json.bak
+
+cd ../expo-package
+npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION"
+sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION"'"|g' -i.bak package.json
+rm package.json.bak
+cd ..
+
+sed -e 's|"version": "[^"]*"|"version": "'"$PACKAGE_VERSION"'"|g' -i.bak src/version.json
+rm src/version.json.bak
+
+git add {expo,native}-package/package.json
+git add src/version.json
+
+git add expo-package/yarn.lock
+git add native-package/yarn.lock
