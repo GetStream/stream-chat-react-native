d52a2eb4c37b9c785fafe0edbbc5b67dbf7a4044
lucas correia <tsirlucas@gmail.com>
2021-07-02 10:08:00 -0300

    chore: use canary publish instead of manual prerelease

--- package/bin/before-tag.sh
+++ package/bin/before-tag.sh
@@ -8,19 +8,20 @@
 set -eux
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION_WITHOUT_SHA=$(echo "$PACKAGE_VERSION" | cut -d"+" -f1)
 
 cd native-package
-npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION"
-sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION"'"|g' -i.bak package.json
+npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION_WITHOUT_SHA"
+sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION_WITHOUT_SHA"'"|g' -i.bak package.json
 rm package.json.bak
 
 cd ../expo-package
-npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION"
-sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION"'"|g' -i.bak package.json
+npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION_WITHOUT_SHA"
+sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION_WITHOUT_SHA"'"|g' -i.bak package.json
 rm package.json.bak
 cd ..
 
-sed -e 's|"version": "[^"]*"|"version": "'"$PACKAGE_VERSION"'"|g' -i.bak src/version.json
+sed -e 's|"version": "[^"]*"|"version": "'"$PACKAGE_VERSION_WITHOUT_SHA"'"|g' -i.bak src/version.json
 rm src/version.json.bak
 
 git add {expo,native}-package/package.json
