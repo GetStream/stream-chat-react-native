d553d84aadcb30a86d35646b6a6e4f2bf831f8d8
lucas correia <tsirlucas@gmail.com>
2021-07-06 17:08:09 -0300

    chore: only bump version for merge and breaking changes commits

--- changelog-preset.config.js
+++ changelog-preset.config.js
@@ -1,11 +1,25 @@
+const mergePRReg = /^Merge pull request #(\d+) from (.*)$/;
+
 module.exports = Promise.resolve()
   .then(() => require('conventional-changelog-angular'))
   .then((preset) => {
     const oldWhatBump = preset.recommendedBumpOpts.whatBump;
-    const newWhatBump = (...params) => {
-      const result = oldWhatBump(...params);
+    const newWhatBump = (commits, packageInfo) => {
+      const parsedCommits = commits
+        // Removes commits that are not pull request merges.
+        .filter(
+          (commit) => mergePRReg.test(commit.header) || commit.footer.includes('BREAKING CHANGE:'),
+        )
+        // For those, sets the body as the header once the actual pull request title is set as body
+        .map((commit) => {
+          const [, type, scope, subject] =
+            commit.body.match(packageInfo.config.parserOpts.headerPattern) || [];
+          return { ...commit, header: commit.body, body: null, type, scope, subject };
+        });
+
+      let result = oldWhatBump(parsedCommits, packageInfo);
       if (result.level === 0) {
-        return {
+        result = {
           level: 1,
           reason: "Major bump replaced with minor in order to match React Native's versioning spec",
         };
