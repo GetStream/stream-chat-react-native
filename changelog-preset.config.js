f2a409354d7c2dbc8fe458010abb24adf1e8ac79
lucas correia <tsirlucas@gmail.com>
2021-07-08 19:50:02 -0300

    chore: fix changelog preset setup

--- changelog-preset.config.js
+++ changelog-preset.config.js
@@ -1,27 +1,35 @@
 const mergePRReg = /^Merge pull request #(\d+) from (.*)$/;
+const semanticReg = /^(\w*)(?:\((.*)\))?: (.*)$/;
+
+const transformCommits = (commits) => {
+  return (
+    commits
+      // Removes commits that are not pull request merges or breaking change.
+      .filter(
+        (commit) =>
+          (mergePRReg.test(commit.header) && semanticReg.test(commit.body)) ||
+          (commit.footer && commit.footer.includes('BREAKING CHANGE:')),
+      )
+      // For those, sets the body as the header once the actual pull request title is set as body
+      .map((commit) => {
+        if (commit.footer && commit.footer.includes('BREAKING CHANGE:')) {
+          return commit;
+        }
+
+        const [, type, scope, subject] = commit.body.match(semanticReg) || [];
+        return { ...commit, header: commit.body, body: null, type, scope, subject };
+      })
+  );
+};
 
 module.exports = Promise.resolve()
   .then(() => require('conventional-changelog-angular'))
   .then((preset) => {
     const oldWhatBump = preset.recommendedBumpOpts.whatBump;
-    const newWhatBump = (commits, packageInfo) => {
-      const parsedCommits = commits
-        // Removes commits that are not pull request merges or breaking change.
-        .filter(
-          (commit) =>
-            mergePRReg.test(commit.header) ||
-            (commit.footer && commit.footer.includes('BREAKING CHANGE:')),
-        )
-        // For those, sets the body as the header once the actual pull request title is set as body
-        .map((commit) => {
-          if (commit.footer && commit.footer.includes('BREAKING CHANGE:')) {
-            return commit;
-          }
+    const oldTransform = preset.conventionalChangelog.writerOpts.transform;
 
-          const [, type, scope, subject] =
-            commit.body.match(packageInfo.config.parserOpts.headerPattern) || [];
-          return { ...commit, header: commit.body, body: null, type, scope, subject };
-        });
+    const newWhatBump = (commits, packageInfo) => {
+      const parsedCommits = transformCommits(commits);
 
       let result = oldWhatBump(parsedCommits, packageInfo);
       if (result.level === 0) {
@@ -33,7 +41,13 @@ module.exports = Promise.resolve()
       return result;
     };
 
+    const newTransform = (commit, context) => {
+      const [parsedCommit] = transformCommits([commit]);
+      return oldTransform(parsedCommit || commit, context);
+    };
+
     preset.recommendedBumpOpts.whatBump = newWhatBump;
+    preset.conventionalChangelog.writerOpts.transform = newTransform;
 
     return preset;
   });
