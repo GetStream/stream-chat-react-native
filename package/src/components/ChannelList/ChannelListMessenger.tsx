1267f64d2f003477e12edbb8b1e833872416510d
lucas correia <tsirlucas@gmail.com>
2021-07-09 13:39:00 -0300

    fix(android): use react native FlatList instead of RNGH's on ChannelListMessenger

--- package/src/components/ChannelList/ChannelListMessenger.tsx
+++ package/src/components/ChannelList/ChannelListMessenger.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { RefreshControl, StyleSheet, View } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, RefreshControl, StyleSheet, View } from 'react-native';
 
 import { ChannelPreview } from '../ChannelPreview/ChannelPreview';
 
