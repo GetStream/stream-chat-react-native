1c6bbf02f3aa1e3ac7b8b338a7a48ab9655263d1
lucas correia <tsirlucas@gmail.com>
2021-07-19 12:52:16 -0300

    chore: fix quotes on prod release file

--- release/prod.js
+++ release/prod.js
@@ -1,17 +1,17 @@
-const semanticRelease = require("semantic-release");
-const configPromise = require("./release.config.js");
+const semanticRelease = require('semantic-release');
+const configPromise = require('./release.config.js');
 
 configPromise.then((config) => {
   const currentPackage = require(`${process.cwd()}/package.json`);
-  const isSDK = currentPackage.name === "stream-chat-react-native-core";
+  const isSDK = currentPackage.name === 'stream-chat-react-native-core';
 
   const newConfig = {
     ...config,
-    branches: ["master"],
+    branches: ['master'],
   };
   if (process.env.GH_TOKEN || process.env.GITHUB_TOKEN) {
     newConfig.plugins.push([
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
         assets: [
           `${process.cwd()}/package.json`,
@@ -19,12 +19,12 @@ configPromise.then((config) => {
           `${process.cwd()}/CHANGELOG.md`,
         ],
         message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ]);
 
     if (isSDK) {
-      plugins.push("@semantic-release/github");
+      plugins.push('@semantic-release/github');
     }
   }
   return semanticRelease(newConfig);
