888b61f89873cc27c8d1f4448640bb39f9257121
lucas correia <tsirlucas@gmail.com>
2021-07-29 15:28:13 -0300

    chore: exit with success in case there is nothing to commit

--- release/prod.js
+++ release/prod.js
@@ -1,22 +1,23 @@
-const semanticRelease = require("semantic-release");
-const configPromise = require("./release.config.js");
+const semanticRelease = require('semantic-release');
+const configPromise = require('./release.config.js');
 
 configPromise.then((config) => {
   const currentPackage = require(`${process.cwd()}/package.json`);
-  const isSDK = currentPackage.name === "stream-chat-react-native-core";
+  const isSDK = currentPackage.name === 'stream-chat-react-native-core';
 
   const newConfig = {
     ...config,
-    branches: ["master"],
+    branches: ['master'],
   };
 
   if (process.env.GH_TOKEN || process.env.GITHUB_TOKEN) {
-    const commitMessage = 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}';
+    const commitMessage =
+      'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}';
 
     newConfig.plugins.push([
       '@semantic-release/exec',
       {
-        prepareCmd: `git diff-index --quiet HEAD -- ${process.cwd()}/* || git add ${process.cwd()}/* && git commit -m "${commitMessage}" && git push origin master`,
+        prepareCmd: `git diff-index --quiet HEAD -- ${process.cwd()}/* && exit 0 || (git add ${process.cwd()}/* && git commit -m "${commitMessage}" && git push origin master)`,
       },
     ]);
 
