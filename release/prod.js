4bdfeb75da145738cdb73ae317487028582cded0
lucas correia <tsirlucas@gmail.com>
2021-07-29 15:38:28 -0300

    chore: turn off addIgnoredFile message

--- release/prod.js
+++ release/prod.js
@@ -17,7 +17,7 @@ configPromise.then((config) => {
     newConfig.plugins.push([
       '@semantic-release/exec',
       {
-        prepareCmd: `git diff-index --quiet HEAD -- ${process.cwd()}/* && exit 0 || (git add ${process.cwd()}/* && git commit -m "${commitMessage}" && git push origin master)`,
+        prepareCmd: `git diff-index --quiet HEAD -- ${process.cwd()}/* && exit 0 || (git config advice.addIgnoredFile false && git add ${process.cwd()}/* && git commit -m "${commitMessage}" && git push origin master)`,
       },
     ]);
 
