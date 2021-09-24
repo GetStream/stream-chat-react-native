b1fd54683e6ee1a5f65d09d0579fafe67455d7b8
Mads Røskar <mads.roskar@getstream.io>
2021-09-24 09:58:52 +0200

    style: Resolve lint errors

--- package/src/components/MessageOverlay/OverlayBackdrop.tsx
+++ package/src/components/MessageOverlay/OverlayBackdrop.tsx
@@ -3,15 +3,15 @@ import { StyleProp, View, ViewStyle } from 'react-native';
 import { useTheme } from '../../contexts/themeContext/ThemeContext';
 
 type OverlayBackdropProps = {
-    style?: StyleProp<ViewStyle>;
+  style?: StyleProp<ViewStyle>;
 };
 
 export const OverlayBackdrop = (props: OverlayBackdropProps): JSX.Element => {
-    const { style = {} } = props;
-    const {
-        theme: {
-            colors: { overlay },
-        },
-    } = useTheme();
-    return <View style={[{ backgroundColor: overlay }, style]} />;
+  const { style = {} } = props;
+  const {
+    theme: {
+      colors: { overlay },
+    },
+  } = useTheme();
+  return <View style={[{ backgroundColor: overlay }, style]} />;
 };
