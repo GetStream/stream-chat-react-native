6e3548f43e7b3ce711f437d2557756c2f5db4b70
vishtree <vishalnarkhede.iitd@gmail.com>
2021-10-22 00:09:06 +0200

    refactor: optimized usIsMounted hook

--- package/src/hooks/useIsMountedRef.ts
+++ package/src/hooks/useIsMountedRef.ts
@@ -1,15 +1,18 @@
 import { useEffect, useRef } from 'react';
 
 export const useIsMountedRef = () => {
-  const isMounted = useRef<boolean | null>(null);
+  // Initial value has been set to true, since this hook exist only for sole purpose of
+  // avoiding any setState calls (within async operation) after component gets unmounted.
+  // Basically we don't need to know about state change from component being initialized -> mounted.
+  // We only need a state change from initialized or mounted state -> unmounted state.
+  const isMounted = useRef(true);
 
-  useEffect(() => {
-    isMounted.current = true;
-
-    return () => {
+  useEffect(
+    () => () => {
       isMounted.current = false;
-    };
-  }, []);
+    },
+    [],
+  );
 
   return isMounted;
 };
