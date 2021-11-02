0b947e2421d2ca1990d98be427d18ed422fefd61
Mads Røskar <mads.roskar@getstream.io>
2021-11-02 18:19:56 +0100

    fix: Check network state separately from cache

--- package/src/components/Channel/Channel.tsx
+++ package/src/components/Channel/Channel.tsx
@@ -113,7 +113,6 @@ import {
   WutReaction,
 } from '../../icons';
 import { FlatList as FlatListDefault } from '../../native';
-import { StreamCache } from '../../StreamCache';
 import { generateRandomId, ReactionData } from '../../utils/utils';
 
 import type { MessageType } from '../MessageList/hooks/useMessageList';
@@ -129,6 +128,7 @@ import type {
   DefaultUserType,
   UnknownType,
 } from '../../types/types';
+import { useNetworkState } from 'src/hooks/useNetworkState';
 
 const styles = StyleSheet.create({
   selectChannel: { fontWeight: 'bold', padding: 16 },
@@ -583,6 +583,8 @@ const ChannelWithContext = <
   const [syncingChannel, setSyncingChannel] = useState(false);
 
   const { setTargetedMessage, targetedMessage } = useTargetedMessage(messageId);
+
+  const { isConnected } = useNetworkState();
 
   const toast = useToastContext();
 
@@ -1330,7 +1332,7 @@ const ChannelWithContext = <
     } as StreamMessage<At, Me, Us>;
 
     try {
-      if (!StreamCache.getInstance().currentNetworkState) {
+      if (!isConnected) {
         console.log(t('Something went wrong'));
         toast.show(t('Something went wrong'), 2000);
         throw new Error('No network connection');
