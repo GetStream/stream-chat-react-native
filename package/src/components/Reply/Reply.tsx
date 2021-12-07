47d3c9a1d31f89373024de0d7b3daf077f6754da
vishtree <vishalnarkhede.iitd@gmail.com>
2021-12-07 16:36:30 +0530

    fix: quote reply preview for video file attachment

--- package/src/components/Reply/Reply.tsx
+++ package/src/components/Reply/Reply.tsx
@@ -125,7 +125,9 @@ const ReplyWithContext = <
   const lastAttachment = quotedMessage.attachments?.slice(-1)[0];
 
   const messageType = lastAttachment
-    ? lastAttachment.type === 'file' || lastAttachment.type === 'audio'
+    ? lastAttachment.type === 'file' ||
+      lastAttachment.type === 'audio' ||
+      lastAttachment.type === 'video'
       ? 'file'
       : lastAttachment.type === 'image' &&
         !lastAttachment.title_link &&
