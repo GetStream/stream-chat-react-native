0834e65fea72ae3a28e311ffa9d313ff41810c57
Petyo Ivanov <underlog@gmail.com>
2021-12-16 12:11:45 +0200

    chore(): fix eslint key order

--- package/jest-setup.js
+++ package/jest-setup.js
@@ -40,9 +40,9 @@ jest.mock('@gorhom/bottom-sheet', () => {
   const react = require('react-native');
   return {
     __esModule: true,
-    default: react.View,
-    BottomSheetScrollView: react.ScrollView,
     BottomSheetFlatList: react.FlatList,
+    BottomSheetScrollView: react.ScrollView,
+    default: react.View,
   };
 });
 // jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
