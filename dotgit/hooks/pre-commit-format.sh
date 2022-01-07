c949eff9fdc14b50099324c24f5f2e6e111e44bf
Mads Røskar <mads.roskar@getstream.io>
2022-01-07 18:25:36 +0100

    build: move packages to a flat structure

--- dotgit/hooks/pre-commit-format.sh
+++ dotgit/hooks/pre-commit-format.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-if ! yarn lerna-workspaces run lint; then
-    yarn lerna-workspaces run lint-fix
-    echo "some files were not formatted correctly (prettier/eslint) commit aborted!"
-    echo "your changes are still staged, you can accept formatting changes with git add or ignore them by adding --no-verify to git commit"
-    exit 1
-fi
+echo "Pre commit format is called"
