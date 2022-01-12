ac9ae21f110582cea15e559b096871bfadcbaa66
Mads Røskar <mads.roskar@getstream.io>
2022-01-12 16:01:50 +0100

    chore: set up jscodeshift

---
+++ transforms/jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  clearMocks: true,
+  coverageProvider: "v8",
+
+  moduleFileExtensions: [
+    "js",
+    "ts",
+    "tsx",
+  ],
+
+  testPathIgnorePatterns: [
+    "/node_modules/"
+  ]
+};
