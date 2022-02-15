3d5ab4c1378e48cfbbe415d893baad9b58d67bd9
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-02-15 14:57:28 +0530

    feat: initial native web support code

---
+++ examples/StreamReactNativeWebApp/index.native.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './src/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ examples/StreamReactNativeWebApp/src/App.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
+import {} from 'stream-chat-react-native-web';
+
+const Section = ({children, title}) => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={[styles.sectionTitle]}>{title}</Text>
+      <Text style={[styles.sectionDescription]}>{children}</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes"></Section>
+          <Section title="Debug"></Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <Button
+            title="Press me"
+            onPress={async () =>
+              await navigator.share({
+                title: 'Hello',
+                text: 'Hey there',
+                url: 'https://www.himgs.com/imagenes/hello/social/hello-fb-logo.png',
+              })
+            }
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
