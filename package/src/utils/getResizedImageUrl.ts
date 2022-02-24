1fe96df6ab708706dad53741486deab7bd399ebb
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2022-02-24 12:26:28 +0100

    refactor: move try catch around entire getResizedImageUrl function

--- package/src/utils/getResizedImageUrl.ts
+++ package/src/utils/getResizedImageUrl.ts
@@ -25,9 +25,30 @@ export function getResizedImageUrl({
   url,
   width,
 }: GetResizedImageUrlParams) {
-  let parsedUrl;
   try {
-    parsedUrl = new URL(url);
+    const parsedUrl = new URL(url);
+
+    const originalHeight = parsedUrl.searchParams.get('oh');
+    const originalWidth = parsedUrl.searchParams.get('ow');
+
+    // If url is not from new cloudfront CDN (which offers fast image resizing), then return the url as it is.
+    // Check for oh and ow parameters in the url, is just to differentiate between old and new CDN.
+    // In case of old CDN we don't want to do any kind of resizing.
+    const isResizableUrl = url.includes('.stream-io-cdn.com') && originalHeight && originalWidth;
+
+    if (!isResizableUrl || (!height && !width)) return url;
+
+    if (height) {
+      parsedUrl.searchParams.set('h', `${PixelRatio.getPixelSizeForLayoutSize(Number(height))}`);
+    }
+
+    if (width) {
+      parsedUrl.searchParams.set('w', `${PixelRatio.getPixelSizeForLayoutSize(Number(width))}`);
+    }
+
+    parsedUrl.searchParams.set('resize', `${resize}`);
+
+    return parsedUrl.toString();
   } catch (error) {
     // There is some issue with the url.
     // Simply return the original url, there is no need to break the app for this.
@@ -35,26 +56,4 @@ export function getResizedImageUrl({
 
     return url;
   }
-
-  const originalHeight = parsedUrl.searchParams.get('oh');
-  const originalWidth = parsedUrl.searchParams.get('ow');
-
-  // If url is not from new cloudfront CDN (which offers fast image resizing), then return the url as it is.
-  // Check for oh and ow parameters in the url, is just to differentiate between old and new CDN.
-  // In case of old CDN we don't want to do any kind of resizing.
-  const isResizableUrl = url.includes('.stream-io-cdn.com') && originalHeight && originalWidth;
-
-  if (!isResizableUrl || (!height && !width)) return url;
-
-  if (height) {
-    parsedUrl.searchParams.set('h', `${PixelRatio.getPixelSizeForLayoutSize(Number(height))}`);
-  }
-
-  if (width) {
-    parsedUrl.searchParams.set('w', `${PixelRatio.getPixelSizeForLayoutSize(Number(width))}`);
-  }
-
-  parsedUrl.searchParams.set('resize', `${resize}`);
-
-  return parsedUrl.toString();
 }
