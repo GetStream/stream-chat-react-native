d6057445c5a8c48a0e75386f029286103c09dcc6
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2022-03-30 13:54:26 +0200

    fix: fix the failing message list test

--- package/src/components/MessageList/__tests__/MessageList.test.js
+++ package/src/components/MessageList/__tests__/MessageList.test.js
@@ -38,7 +38,7 @@ describe('MessageList', () => {
       <OverlayProvider>
         <Chat client={chatClient}>
           <Channel channel={channel}>
-            <MessageList />
+            <MessageList hasNoMoreRecentMessagesToLoad />
           </Channel>
         </Chat>
       </OverlayProvider>,
