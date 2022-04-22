a4b24aec9086ba73f33a0916d277be77d1ba7b71
Mads Røskar <madshvero@gmail.com>
2022-04-22 21:08:53 +0200

    fix: set border radius to 16 for giphy selector

--- package/src/components/Attachment/Giphy.tsx
+++ package/src/components/Attachment/Giphy.tsx
@@ -65,6 +65,7 @@ const styles = StyleSheet.create({
   },
   giphyHeaderTitle: {
     fontSize: 14,
+    marginLeft: 8,
   },
   giphyMask: {
     bottom: 8,
@@ -84,7 +85,8 @@ const styles = StyleSheet.create({
     width: '60%',
   },
   selectionContainer: {
-    borderBottomLeftRadius: 8,
+    borderRadius: 16,
+    borderBottomRightRadius: 0,
     borderWidth: 1,
     overflow: 'hidden',
     width: 272,
