ee3fd35c6280e231a1e198ee50cc279879e1f366
Nwagba Okechukwu Martin <nwagba001@gmail.com>
2022-05-03 16:32:18 +0100

    fix: rectify indicatorStates type

--- package/src/utils/utils.ts
+++ package/src/utils/utils.ts
@@ -60,11 +60,11 @@ export const MessageStatusTypes = {
 export type UploadState = 'upload_failed' | 'not_supported' | 'uploaded' | 'finished' | 'uploading';
 export type ProgressIndicatorState = 'in_progress' | 'retry' | 'not_supported' | 'inactive' | null;
 export type IndicatorStates = {
-  uploading: 'in_progress';
-  upload_failed: 'retry';
-  not_supported: 'not_supported';
-  uploaded: 'inactive';
-  finished: 'inactive';
+  'uploading': 'in_progress';
+  'upload_failed': 'retry';
+  'not_supported': 'not_supported';
+  'uploaded': 'inactive';
+  'finished': 'inactive';
 };
 export const getIndicatorTypeForFileState = (fileState: UploadState): ProgressIndicatorState => {
   const indicatorMap: IndicatorStates = {
