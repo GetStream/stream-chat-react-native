ddacd2c5350b1280918c5167f4011ed8bba13924
Nwagba Okechukwu Martin <nwagba001@gmail.com>
2022-05-04 01:49:16 +0100

    fix: again, rectify indicatorStates by remove string and adding ProgressIndicatorState as value in type def

--- package/src/utils/utils.ts
+++ package/src/utils/utils.ts
@@ -59,15 +59,12 @@ export const MessageStatusTypes = {
 
 export type UploadState = 'upload_failed' | 'not_supported' | 'uploaded' | 'finished' | 'uploading';
 export type ProgressIndicatorState = 'in_progress' | 'retry' | 'not_supported' | 'inactive' | null;
-export type IndicatorStates = {
-  'uploading': 'in_progress';
-  'upload_failed': 'retry';
-  'not_supported': 'not_supported';
-  'uploaded': 'inactive';
-  'finished': 'inactive';
+export type IndicatorStatesMap = {
+  [x: string]: ProgressIndicatorState;
 };
+
 export const getIndicatorTypeForFileState = (fileState: UploadState): ProgressIndicatorState => {
-  const indicatorMap: IndicatorStates = {
+  const indicatorMap: IndicatorStatesMap = {
     [FileState.UPLOADING]: ProgressIndicatorTypes.IN_PROGRESS,
     [FileState.UPLOAD_FAILED]: ProgressIndicatorTypes.RETRY,
     [FileState.NOT_SUPPORTED]: ProgressIndicatorTypes.NOT_SUPPORTED,
