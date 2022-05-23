ab0bb869b34e48950e4c4d8d08a22c05592cac07
Nwagba Okechukwu <enigma_i_am@Nwagbas-MacBook-Pro.local>
2022-05-23 17:11:18 +0100

    refactor: removed redundant test

--- package/src/components/ChannelPreview/hooks/__tests__/useChannelPreviewDisplayName.test.tsx
+++ package/src/components/ChannelPreview/hooks/__tests__/useChannelPreviewDisplayName.test.tsx
@@ -22,6 +22,7 @@ describe('useChannelPreviewDisplayName', () => {
   const clientUser = generateUser();
   let chatClient: StreamChat<DefaultGenerics> | StreamChat<DefaultStreamChatGenerics>;
   let channel: Channel<DefaultGenerics> | Channel<DefaultStreamChatGenerics> | null;
+  const CHARACTER_LENGTH = 15;
 
   beforeEach(async () => {
     chatClient = await getTestClientWithUser(clientUser);
@@ -32,7 +33,6 @@ describe('useChannelPreviewDisplayName', () => {
   });
 
   it('should return a channel display name', async () => {
-    const characterLength = 15;
     const channelName = 'okechukwu';
 
     const TestComponent = () => {
@@ -40,7 +40,7 @@ describe('useChannelPreviewDisplayName', () => {
         {
           data: { name: channelName },
         } as unknown as Channel<DefaultGenerics>,
-        characterLength,
+        CHARACTER_LENGTH,
       );
       return <Text>{channelDisplayName}</Text>;
     };
@@ -54,12 +54,11 @@ describe('useChannelPreviewDisplayName', () => {
 
   it('should return the full channelName when channelName length is less than characterLength', () => {
     const channelName = 'okechukwu';
-    const characterLength = 15;
     const currentUserId = chatClient.userID;
 
     const displayName = getChannelPreviewDisplayName({
       channelName,
-      characterLimit: characterLength,
+      characterLimit: CHARACTER_LENGTH,
       currentUserId,
       members: channel?.state.members,
     });
@@ -67,28 +66,10 @@ describe('useChannelPreviewDisplayName', () => {
     expect(displayName).toEqual(channelName);
   });
 
-  it('should return the first characters of a channelName up to a limit of characterLength', () => {
-    const channelName = 'okechukwu nwagba martin';
-    const characterLength = 15;
-    const currentUserId = chatClient.userID;
-
-    const displayName = getChannelPreviewDisplayName({
-      channelName,
-      characterLimit: characterLength,
-      currentUserId,
-      members: channel?.state.members as unknown as Record<
-        string,
-        ChannelMemberResponse<DefaultStreamChatGenerics>
-      >,
-    });
-
-    expect(displayName).toEqual(channelName);
-  });
-
   it.each([
-    [15, GROUP_CHANNEL_MEMBERS_MOCK, 'okechukwu nwagba', 'ben, nick, q,...+2'],
-    [15, ONE_CHANNEL_MEMBER_MOCK, 'okechukwu nwagba', 'okechukwu nw...'],
-    [15, ONE_MEMBER_WITH_EMPTY_USER_MOCK, 'okechukwu nwagba', 'Unknown User...'],
+    [CHARACTER_LENGTH, GROUP_CHANNEL_MEMBERS_MOCK, 'okechukwu nwagba', 'ben, nick, q,...+2'],
+    [CHARACTER_LENGTH, ONE_CHANNEL_MEMBER_MOCK, 'okechukwu nwagba', 'okechukwu nw...'],
+    [CHARACTER_LENGTH, ONE_MEMBER_WITH_EMPTY_USER_MOCK, 'okechukwu nwagba', 'Unknown User...'],
   ])(
     'getChannelPreviewDisplayName(%i, %p, %s) result in %s',
     (characterLength, members, currentUserId, expected) => {
