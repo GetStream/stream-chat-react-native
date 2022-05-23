3dc41d4664e147fcb969c1f36cdfc8ff254d1481
Nwagba Okechukwu <enigma_i_am@Nwagbas-MacBook-Pro.local>
2022-05-23 17:03:23 +0100

    refactor: removed variable typing and inline comments

--- package/src/components/ChannelPreview/hooks/useChannelPreviewDisplayName.ts
+++ package/src/components/ChannelPreview/hooks/useChannelPreviewDisplayName.ts
@@ -27,13 +27,10 @@ export const getChannelPreviewDisplayName = <
   currentUserId?: string;
   members?: Channel<StreamChatGenerics>['state']['members'];
 }): string => {
-  // return channelName if it exists
   if (channelName) return channelName;
 
-  // turn the values of the members object to a list
   const channelMembers = Object.values(members || {});
 
-  // filter the list to remove the current user
   const otherMembers = channelMembers.filter((member) => member.user?.id !== currentUserId);
   otherMembers.sort((prevUser, nextUser) =>
     (prevUser?.user?.name ?? '')
@@ -49,15 +46,15 @@ export const getChannelPreviewDisplayName = <
       return result;
     }
 
-    const currentMemberName: string = getMemberName(currentMember);
+    const currentMemberName = getMemberName(currentMember);
 
-    const resultHasSpaceForCurrentMemberName: boolean =
+    const resultHasSpaceForCurrentMemberName =
       result.length + (currentMemberName.length + ELLIPSIS.length) < characterLimit;
     if (resultHasSpaceForCurrentMemberName) {
       return result.length > 0 ? `${result}, ${currentMemberName}, ` : currentMemberName;
     } else {
       const remainingNumberOfMembers = originalArray.length - index;
-      const truncateLimit: number = characterLimit - (ELLIPSIS.length + result.length);
+      const truncateLimit = characterLimit - (ELLIPSIS.length + result.length);
       const tuncatedCurrentMemberName = `${currentMemberName.slice(0, truncateLimit)}`;
 
       const channelNameSuffix = createChannelNameSuffix(remainingNumberOfMembers);
