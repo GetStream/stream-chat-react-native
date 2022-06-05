10587cc0197afeea0051ea0cce2fa5e9bf2d352d
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2022-06-05 12:11:04 +0200

    refactor: make openUrlSafely function async

--- package/src/components/Attachment/utils/openUrlSafely.ts
+++ package/src/components/Attachment/utils/openUrlSafely.ts
@@ -1,18 +1,17 @@
 import { Linking } from 'react-native';
 
-export const openUrlSafely = (url?: string) => {
+export const openUrlSafely = async (url?: string) => {
   let finalUrl = url as string;
   const pattern = new RegExp(/^\S+:\/\//);
 
   if (!pattern.test(finalUrl)) {
     finalUrl = 'http://' + url;
   }
+  const supported = await Linking.canOpenURL(finalUrl);
 
-  Linking.canOpenURL(finalUrl).then((supported) => {
-    if (supported) {
-      Linking.openURL(finalUrl);
-    } else {
-      console.warn(`Don't know how to open URI: ${finalUrl}`);
-    }
-  });
+  if (supported) {
+    Linking.openURL(finalUrl);
+  } else {
+    console.warn(`Don't know how to open URI: ${finalUrl}`);
+  }
 };
