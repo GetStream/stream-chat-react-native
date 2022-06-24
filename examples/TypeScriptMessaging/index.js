c1fbd052e13013ecf0e9081b0a8b66bf43e797fb
Nwagba Okechukwu <enigma_i_am@Nwagbas-MacBook-Pro.local>
2022-06-24 14:27:04 +0100

    refactor: remove enforced rtl in index.js

--- examples/TypeScriptMessaging/index.js
+++ examples/TypeScriptMessaging/index.js
@@ -1,9 +1,7 @@
 import 'react-native-gesture-handler';
-import { AppRegistry, I18nManager } from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
-
-I18nManager.forceRTL(true);
 
 AppRegistry.registerComponent(appName, () => App);
