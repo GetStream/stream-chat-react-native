63e7bf899eaa3d77d694be86fa31e01106b26525
Nwagba Okechukwu <enigma_i_am@Nwagbas-MacBook-Pro.local>
2022-06-29 12:04:58 +0100

    fix: asserting only previews in useLatestMessagePreview.test.tsx

--- package/src/components/ChannelPreview/hooks/__tests__/useLatestMessagePreview.test.tsx
+++ package/src/components/ChannelPreview/hooks/__tests__/useLatestMessagePreview.test.tsx
@@ -54,7 +54,7 @@ describe('useLatestMessagePreview', () => {
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toBeTruthy();
+      expect(result.current.previews).toBeTruthy();
     });
   });
 
@@ -64,254 +64,96 @@ describe('useLatestMessagePreview', () => {
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'L',
-        messageObject: {
-          created_at: new Date('2021-02-12T12:12:35.862Z'),
-          id: '',
-          message: {},
-          pinned_at: new Date('2021-02-12T12:12:35.862Z'),
-          status: 'received',
-          updated_at: new Date('2021-02-12T12:12:35.862Z'),
-        },
-        previews: [
-          { bold: false, text: '' },
-          { bold: false, text: 'Empty message...' },
-        ],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([
+        { bold: false, text: '' },
+        { bold: false, text: 'Empty message...' },
+      ]);
     });
   });
 
-  it('should return a nothing yet', async () => {
-    const { result } = renderHook(
-      () => useLatestMessagePreview(CHANNEL_WITH_NO_MESSAGES, FORCE_UPDATE, LATEST_MESSAGE),
-      { wrapper: ChatProvider },
-    );
-    await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'LT',
-        previews: [{ bold: false, text: 'Nothing yet...' }],
-        status: 0,
-      });
-    });
-  });
-
-  it('should return a deleted message', async () => {
+  it('should return a deleted message preview', async () => {
     const { result } = renderHook(
       () => useLatestMessagePreview(CHANNEL_WITH_DELETED_MESSAGES, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'LT',
-        messageObject: {
-          type: 'deleted',
-        },
-        previews: [{ bold: false, text: 'Message deleted' }],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([{ bold: false, text: 'Message deleted' }]);
     });
   });
 
-  it('should return message deleted', async () => {
+  it('should return an "Nothing yet..." message preview', async () => {
     const { result } = renderHook(
       () => useLatestMessagePreview(CHANNEL_WITH_NO_MESSAGES, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'LT',
-        previews: [{ bold: false, text: 'Nothing yet...' }],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([{ bold: false, text: 'Nothing yet...' }]);
     });
   });
 
-  it('should return latest text', async () => {
+  it('should return latest message preview', async () => {
     const { result } = renderHook(
       () => useLatestMessagePreview(CHANNEL_WITH_MESSAGES_TEXT, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'L',
-        messageObject: {
-          args: 'string',
-          attachments: [],
-          channel: { data: { name: 'okechukwu' }, state: { messages: [] } },
-          cid: 'stridodong',
-          command: 'giphy',
-          command_info: {
-            name: 'string',
-          },
-          created_at: new Date('2021-02-12T12:12:35.862Z'),
-          deleted_at: new Date('2021-02-12T12:12:35.862Z'),
-          id: 'jbkjb',
-          text: 'jkbkbiubicbi',
-          type: 'MessageLabel',
-          user: {
-            id: 'okechukwu',
-          },
-        },
-        previews: [
-          { bold: true, text: '@okechukwu: ' },
-          { bold: false, text: 'jkbkbiubicbi' },
-        ],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([
+        { bold: true, text: '@okechukwu: ' },
+        { bold: false, text: 'jkbkbiubicbi' },
+      ]);
     });
   });
 
-  it('should return a channel with an empty message', async () => {
+  it('should return a channel with an empty message preview', async () => {
     const { result } = renderHook(
       () => useLatestMessagePreview(CHANNEL_WITH_EMPTY_MESSAGE, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'L',
-        messageObject: {
-          args: 'string',
-          attachments: [],
-          cid: 'stridodong',
-          command_info: {
-            name: 'string',
-          },
-          created_at: new Date('2021-02-12T12:12:35.862Z'),
-          deleted_at: new Date('2021-02-12T12:12:35.862Z'),
-          mentioned_users: [
-            { id: 'Max', name: 'Max' },
-            { id: 'Ada', name: 'Ada' },
-            { id: 'Enzo', name: 'Enzo' },
-          ],
-        },
-        previews: [
-          { bold: false, text: '' },
-          { bold: false, text: 'Empty message...' },
-        ],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([
+        { bold: false, text: '' },
+        { bold: false, text: 'Empty message...' },
+      ]);
     });
   });
 
-  it('should return a channel with a mentioned user', async () => {
+  it('should return a mentioned user (@Max) message preview', async () => {
     const { result } = renderHook(
       () => useLatestMessagePreview(CHANNEL_WITH_MENTIONED_USERS, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'L',
-        messageObject: {
-          args: 'string',
-          attachments: [],
-          cid: 'stridodong',
-          command_info: {
-            name: 'string',
-          },
-          created_at: new Date('2021-02-12T12:12:35.862Z'),
-          deleted_at: new Date('2021-02-12T12:12:35.862Z'),
-          mentioned_users: [
-            { id: 'Max', name: 'Max' },
-            { id: 'Ada', name: 'Ada' },
-            { id: 'Enzo', name: 'Enzo' },
-          ],
-          text: 'Max',
-        },
-        previews: [
-          { bold: false, text: '' },
-          { bold: false, text: 'Max' },
-        ],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([
+        { bold: false, text: '' },
+        { bold: false, text: 'Max' },
+      ]);
     });
   });
 
-  it('should return latest command', async () => {
+  it('should return the latest command preview', async () => {
     const { result } = renderHook(
       () => useLatestMessagePreview(CHANNEL_WITH_MESSAGES_COMMAND, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'L',
-        messageObject: {
-          args: 'string',
-          attachments: [],
-          channel: { data: { name: 'okechukwu' }, state: { messages: [] } },
-          cid: 'stridodong',
-          command: 'giphy',
-          command_info: {
-            name: 'string',
-          },
-          created_at: new Date('2021-02-12T12:12:35.862Z'),
-          deleted_at: new Date('2021-02-12T12:12:35.862Z'),
-          id: 'jbkjb',
-          user: {
-            id: 'okechukwu',
-          },
-        },
-        previews: [
-          { bold: true, text: '@okechukwu: ' },
-          { bold: false, text: '/giphy' },
-        ],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([
+        { bold: true, text: '@okechukwu: ' },
+        { bold: false, text: '/giphy' },
+      ]);
     });
   });
 
-  it('should return latest attachment', async () => {
+  it('should return an attachment preview', async () => {
     const { result } = renderHook(
       () =>
         useLatestMessagePreview(CHANNEL_WITH_MESSAGES_ATTACHMENTS, FORCE_UPDATE, LATEST_MESSAGE),
       { wrapper: ChatProvider },
     );
     await waitFor(() => {
-      expect(result.current).toEqual({
-        created_at: 'L',
-        messageObject: {
-          args: 'string',
-          attachments: [
-            {
-              actions: [],
-              asset_url: 'string',
-              author_icon: 'string',
-              author_link: 'string',
-              author_name: 'string',
-              color: 'string',
-              fallback: 'string',
-              fields: [],
-              file_size: 25,
-              footer: 'string',
-              footer_icon: 'string',
-              image_url: 'string',
-              mime_type: 'string',
-              og_scrape_url: 'string',
-              original_height: 5,
-              original_width: 4,
-              pretext: 'string',
-              text: 'string',
-              thumb_url: 'string',
-              title: 'string',
-              title_link: 'string',
-              type: 'string',
-            },
-          ],
-          channel: { data: { name: 'okechukwu' }, state: { messages: [] } },
-          created_at: new Date('2021-02-12T12:12:35.862Z'),
-          deleted_at: new Date('2021-02-12T12:12:35.862Z'),
-          id: 'ljkblk',
-          user: {
-            id: 'okechukwu',
-          },
-        },
-        previews: [
-          { bold: true, text: '@okechukwu: ' },
-          { bold: false, text: '🏙 Attachment...' },
-        ],
-        status: 0,
-      });
+      expect(result.current.previews).toEqual([
+        { bold: true, text: '@okechukwu: ' },
+        { bold: false, text: '🏙 Attachment...' },
+      ]);
     });
   });
 });
