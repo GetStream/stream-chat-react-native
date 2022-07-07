f15b4542f4c398b932b97c3cefd055071476f489
stevegalili <galiliziv@gmail.com>
2022-07-07 16:45:47 +0200

    fix: tutorial works with R18 and RN69, forking markdown lib

--- package/src/contexts/overlayContext/OverlayProvider.tsx
+++ package/src/contexts/overlayContext/OverlayProvider.tsx
@@ -1,5 +1,43 @@
-import React, { ReactChildren } from 'react';
-import { View } from 'react-native';
+import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
+
+import { BackHandler, Dimensions, StyleSheet, ViewStyle } from 'react-native';
+
+import Animated, {
+  cancelAnimation,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+import type BottomSheet from '@gorhom/bottom-sheet';
+import Dayjs from 'dayjs';
+
+import { OverlayContext, OverlayProviderProps } from './OverlayContext';
+
+import { AttachmentPicker } from '../../components/AttachmentPicker/AttachmentPicker';
+
+import { AttachmentPickerBottomSheetHandle as DefaultAttachmentPickerBottomSheetHandle } from '../../components/AttachmentPicker/components/AttachmentPickerBottomSheetHandle';
+import { AttachmentPickerError as DefaultAttachmentPickerError } from '../../components/AttachmentPicker/components/AttachmentPickerError';
+import { AttachmentPickerErrorImage as DefaultAttachmentPickerErrorImage } from '../../components/AttachmentPicker/components/AttachmentPickerErrorImage';
+import { CameraSelectorIcon as DefaultCameraSelectorIcon } from '../../components/AttachmentPicker/components/CameraSelectorIcon';
+import { FileSelectorIcon as DefaultFileSelectorIcon } from '../../components/AttachmentPicker/components/FileSelectorIcon';
+import { ImageOverlaySelectedComponent as DefaultImageOverlaySelectedComponent } from '../../components/AttachmentPicker/components/ImageOverlaySelectedComponent';
+import { ImageSelectorIcon as DefaultImageSelectorIcon } from '../../components/AttachmentPicker/components/ImageSelectorIcon';
+import { ImageGallery } from '../../components/ImageGallery/ImageGallery';
+import { MessageOverlay } from '../../components/MessageOverlay/MessageOverlay';
+import { OverlayBackdrop } from '../../components/MessageOverlay/OverlayBackdrop';
+import { ChannelsStateProvider } from '../../contexts/channelsStateContext/ChannelsStateContext';
+import { useStreami18n } from '../../hooks/useStreami18n';
+
+import type { DefaultStreamChatGenerics } from '../../types/types';
+import { AttachmentPickerProvider } from '../attachmentPickerContext/AttachmentPickerContext';
+import { ImageGalleryProvider } from '../imageGalleryContext/ImageGalleryContext';
+import { MessageOverlayProvider } from '../messageOverlayContext/MessageOverlayContext';
+import { ThemeProvider } from '../themeContext/ThemeContext';
+import {
+  TranslationContextValue,
+  TranslationProvider,
+} from '../translationContext/TranslationContext';
 
 /**
  * - The highest level of these components is the `OverlayProvider`. The `OverlayProvider` allows users to interact with messages on long press above the underlying views, use the full screen image viewer, and use the `AttachmentPicker` as a keyboard-esk view.
@@ -21,8 +59,185 @@ import { View } from 'react-native';
  *
  * @example ./OverlayProvider.md
  */
-export const OverlayProvider = (props: { children: ReactChildren }) => {
-  const { children } = props;
+export const OverlayProvider = <
+  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics,
+  >(
+  props: PropsWithChildren<OverlayProviderProps<StreamChatGenerics>>,
+) => {
+  const {
+    AttachmentPickerBottomSheetHandle = DefaultAttachmentPickerBottomSheetHandle,
+    attachmentPickerBottomSheetHandleHeight,
+    attachmentPickerBottomSheetHeight,
+    AttachmentPickerError = DefaultAttachmentPickerError,
+    attachmentPickerErrorButtonText,
+    AttachmentPickerErrorImage = DefaultAttachmentPickerErrorImage,
+    attachmentPickerErrorText,
+    attachmentSelectionBarHeight,
+    bottomInset,
+    CameraSelectorIcon = DefaultCameraSelectorIcon,
+    children,
+    closePicker = (ref) => {
+      if (ref.current) {
+        ref.current.close();
+      }
+    },
+    FileSelectorIcon = DefaultFileSelectorIcon,
+    giphyVersion,
+    i18nInstance,
+    imageGalleryCustomComponents,
+    imageGalleryGridHandleHeight,
+    imageGalleryGridSnapPoints,
+    ImageOverlaySelectedComponent = DefaultImageOverlaySelectedComponent,
+    ImageSelectorIcon = DefaultImageSelectorIcon,
+    MessageActionList,
+    MessageActionListItem,
+    messageTextNumberOfLines,
+    numberOfAttachmentImagesToLoadPerCall,
+    numberOfAttachmentPickerImageColumns,
+    numberOfImageGalleryGridColumns,
+    openPicker = (ref) => {
+      if (ref.current?.snapToIndex) {
+        ref.current.snapToIndex(0);
+      } else {
+        console.warn('bottom and top insets must be set for the image picker to work correctly');
+      }
+    },
+    topInset,
+    translucentStatusBar,
+    OverlayReactionList,
+    OverlayReactions,
+    OverlayReactionsAvatar,
+    value,
+  } = props;
 
-  return <View key={1}>{children}</View>;
+  const attachmentPickerProps = {
+    AttachmentPickerBottomSheetHandle,
+    attachmentPickerBottomSheetHandleHeight,
+    attachmentPickerBottomSheetHeight,
+    AttachmentPickerError,
+    attachmentPickerErrorButtonText,
+    AttachmentPickerErrorImage,
+    attachmentPickerErrorText,
+    attachmentSelectionBarHeight,
+    ImageOverlaySelectedComponent,
+    numberOfAttachmentImagesToLoadPerCall,
+    numberOfAttachmentPickerImageColumns,
+    translucentStatusBar,
+  };
+
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const [translators, setTranslators] = useState<TranslationContextValue>({
+    t: (key: string) => key,
+    tDateTimeParser: (input?: string | number | Date) => Dayjs(input),
+  });
+  const [overlay, setOverlay] = useState(value?.overlay || 'none');
+
+  const overlayOpacity = useSharedValue(0);
+  const { height, width } = Dimensions.get('screen');
+
+  // Setup translators
+  const loadingTranslators = useStreami18n({ i18nInstance, setTranslators });
+
+  useEffect(() => {
+    const backAction = () => {
+      if (overlay !== 'none') {
+        setOverlay('none');
+        return true;
+      }
+
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => backHandler.remove();
+  }, [overlay]);
+
+  useEffect(() => {
+    if (bottomSheetRef.current) {
+      bottomSheetRef.current.close?.();
+    }
+    cancelAnimation(overlayOpacity);
+    if (overlay !== 'none') {
+      overlayOpacity.value = withTiming(1);
+    } else {
+      overlayOpacity.value = withTiming(0);
+    }
+  }, [overlay]);
+
+  // Setup translators
+  useStreami18n({ i18nInstance, setTranslators });
+
+  const attachmentPickerContext = {
+    attachmentPickerBottomSheetHeight,
+    attachmentSelectionBarHeight,
+    bottomInset,
+    CameraSelectorIcon,
+    closePicker: () => closePicker(bottomSheetRef),
+    FileSelectorIcon,
+    ImageSelectorIcon,
+    openPicker: () => openPicker(bottomSheetRef),
+    topInset,
+  };
+
+  const overlayStyle = useAnimatedStyle<ViewStyle>(
+    () => ({
+      opacity: overlayOpacity.value,
+    }),
+    [],
+  );
+
+  const overlayContext = {
+    overlay,
+    setOverlay,
+    style: value?.style,
+    translucentStatusBar,
+  };
+
+  if (loadingTranslators) return null;
+
+  return (
+    <TranslationProvider value={translators}>
+      <OverlayContext.Provider value={overlayContext}>
+        <MessageOverlayProvider<StreamChatGenerics>>
+          <AttachmentPickerProvider value={attachmentPickerContext}>
+            <ImageGalleryProvider>
+              <ChannelsStateProvider<StreamChatGenerics>>{children}</ChannelsStateProvider>
+              <ThemeProvider style={overlayContext.style}>
+                <Animated.View
+                  pointerEvents={overlay === 'none' ? 'none' : 'auto'}
+                  style={[StyleSheet.absoluteFill, overlayStyle]}
+                >
+                  <OverlayBackdrop style={[StyleSheet.absoluteFill, { height, width }]} />
+                </Animated.View>
+                {overlay === 'message' && (
+                  <MessageOverlay<StreamChatGenerics>
+                    MessageActionList={MessageActionList}
+                    MessageActionListItem={MessageActionListItem}
+                    messageTextNumberOfLines={messageTextNumberOfLines}
+                    overlayOpacity={overlayOpacity}
+                    OverlayReactionList={OverlayReactionList}
+                    OverlayReactions={OverlayReactions}
+                    OverlayReactionsAvatar={OverlayReactionsAvatar}
+                  />
+                )}
+                {overlay === 'gallery' && (
+                  <ImageGallery<StreamChatGenerics>
+                    giphyVersion={giphyVersion}
+                    imageGalleryCustomComponents={imageGalleryCustomComponents}
+                    imageGalleryGridHandleHeight={imageGalleryGridHandleHeight}
+                    imageGalleryGridSnapPoints={imageGalleryGridSnapPoints}
+                    numberOfImageGalleryGridColumns={numberOfImageGalleryGridColumns}
+                    overlayOpacity={overlayOpacity}
+                  />
+                )}
+                <AttachmentPicker ref={bottomSheetRef} {...attachmentPickerProps} />
+              </ThemeProvider>
+            </ImageGalleryProvider>
+          </AttachmentPickerProvider>
+        </MessageOverlayProvider>
+      </OverlayContext.Provider>
+    </TranslationProvider>
+  );
 };
