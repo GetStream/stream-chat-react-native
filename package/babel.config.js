d4f7c21e84d1709b1a72716c2ddb031a8a304e52
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-07-19 12:41:26 +0530

    fix: overwriting of the translation files when running test or build script

--- package/babel.config.js
+++ package/babel.config.js
@@ -9,26 +9,7 @@ module.exports = (api) => {
           compact: false,
         },
       ],
-      plugins: [
-        [
-          'i18next-extract',
-          {
-            contextSeparator: '__',
-            defaultContexts: [''],
-            defaultNS: 'en',
-            discardOldKeys: true,
-            jsonSpace: 4,
-            keyAsDefaultValue: ['en'],
-            keyAsDefaultValueForDerivedKeys: false,
-            keySeparator: null,
-            locales: ['en', 'fr', 'hi', 'it', 'nl', 'ru', 'tr'],
-            nsSeparator: null,
-            outputPath: 'src/i18n/{{locale}}.json',
-          },
-        ],
-        'module-resolver',
-        'react-native-reanimated/plugin',
-      ],
+      plugins: ['module-resolver', 'react-native-reanimated/plugin'],
       presets: ['@babel/env', 'module:metro-react-native-babel-preset', '@babel/preset-typescript'],
     };
   }
@@ -51,25 +32,7 @@ module.exports = (api) => {
         compact: false,
       },
     ],
-    plugins: [
-      [
-        'i18next-extract',
-        {
-          contextSeparator: '__',
-          defaultContexts: [''],
-          defaultNS: 'en',
-          discardOldKeys: true,
-          jsonSpace: 4,
-          keyAsDefaultValue: ['en'],
-          keyAsDefaultValueForDerivedKeys: false,
-          keySeparator: null,
-          locales: ['nl', 'en', 'it', 'tr', 'fr', 'hi', 'ru'],
-          nsSeparator: null,
-          outputPath: 'src/i18n/{{locale}}.json',
-        },
-      ],
-      'module-resolver',
-    ],
+    plugins: ['module-resolver'],
     presets: ['@babel/env', 'module:metro-react-native-babel-preset', '@babel/preset-typescript'],
   };
 };
