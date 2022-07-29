df7ec222feda894a789416dc8855eafb242c32a7
stevegalili <galiliziv@gmail.com>
2022-07-29 16:28:45 +0200

    refactor: SA android working with rn 0.69.3

--- examples/SampleApp/android/app/src/main/java/com/sampleapp/MainActivity.java
+++ examples/SampleApp/android/app/src/main/java/com/sampleapp/MainActivity.java
@@ -23,17 +23,10 @@ public class MainActivity extends ReactActivity {
    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
    * (Paper).
    */
-
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
+    return new MainActivityDelegate(this, getMainComponentName());
   }
-
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
       super(activity, mainComponentName);
--- examples/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
+++ examples/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
@@ -9,7 +9,9 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.sampleapp.newarchitecture.MainApplicationReactNativeHost;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
