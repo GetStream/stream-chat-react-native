44ecf98cc59ff4701dfd95e725ecd68231ffa3c0
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2022-08-04 12:05:50 +0200

    fix: allow overriding message from props on gallery component (#1620)

--- package/src/components/Attachment/Gallery.tsx
+++ package/src/components/Attachment/Gallery.tsx
@@ -580,6 +580,7 @@ export const Gallery = <
     ImageLoadingFailedIndicator: PropImageLoadingFailedIndicator,
     ImageLoadingIndicator: PropImageLoadingIndicator,
     images: propImages,
+    message: propMessage,
     onLongPress: propOnLongPress,
     onPress: propOnPress,
     onPressIn: propOnPressIn,
@@ -596,7 +597,7 @@ export const Gallery = <
     alignment: contextAlignment,
     groupStyles: contextGroupStyles,
     images: contextImages,
-    message,
+    message: contextMessage,
     onLongPress: contextOnLongPress,
     onPress: contextOnPress,
     onPressIn: contextOnPressIn,
@@ -615,6 +616,7 @@ export const Gallery = <
 
   const images = propImages || contextImages;
   const videos = propVideos || contextVideos;
+  const message = propMessage || contextMessage;
 
   if (!images.length && !videos.length) return null;
 
