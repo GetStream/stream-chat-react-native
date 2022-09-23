eb9a3d8d75d1ed6ea8422730b3c8ae415a83795f
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-09-23 12:01:41 +0530

    fix: add memoization in ReactionList to update MessageOverlay in Real time (#1737)

--- package/src/components/Message/hooks/useCreateMessageContext.ts
+++ package/src/components/Message/hooks/useCreateMessageContext.ts
@@ -48,11 +48,11 @@ export const useCreateMessageContext = <
   const reactionsValue = reactions.map(({ own, type }) => `${own}${type}`).join();
   const latestReactions = message.latest_reactions ? message.latest_reactions : undefined;
   const readBy = isMessageWithStylesReadByAndDateSeparator(message) && message.readBy;
-  const messageValue = `${latestReactions ? latestReactions.map(({ type }) => type).join() : ''}${
-    message.updated_at
-  }${message.deleted_at}${readBy}${message.status}${message.type}${message.text}${
-    message.reply_count
-  }`;
+  const messageValue = `${
+    latestReactions ? latestReactions.map(({ type, user }) => `${type}${user?.id}`).join() : ''
+  }${message.updated_at}${message.deleted_at}${readBy}${message.status}${message.type}${
+    message.text
+  }${message.reply_count}`;
   const membersValue = JSON.stringify(members);
 
   const quotedMessageDeletedValue = message.quoted_message?.deleted_at;
