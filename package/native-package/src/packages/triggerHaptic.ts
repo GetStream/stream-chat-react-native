b76a96fc4d4394aff4b345fda38287827434861d
Mads Røskar <madshvero@gmail.com>
2022-09-29 18:44:33 +0200

    feat: make react-native-haptic-feedback optional

--- package/native-package/src/packages/triggerHaptic.ts
+++ package/native-package/src/packages/triggerHaptic.ts
@@ -1,8 +1,46 @@
-import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+let ReactNativeHapticFeedback;
 
-export const triggerHaptic = (method: ReactNativeHapticFeedback.HapticFeedbackTypes) => {
-  ReactNativeHapticFeedback.trigger(method, {
-    enableVibrateFallback: false,
-    ignoreAndroidSystemSettings: false,
-  });
-};
+try {
+  ReactNativeHapticFeedback = require('react-native-haptic-feedback');
+} catch (e) {
+  console.warn('react-native-haptic-feedback is not installed.');
+}
+
+/**
+ * Since react-native-haptic-feedback isn't installed by default, we've
+ * copied the types from the package here.
+ *
+ * @see https://github.com/junina-de/react-native-haptic-feedback/blob/master/index.d.ts
+ * */
+export type HapticFeedbackTypes =
+  | 'selection'
+  | 'impactLight'
+  | 'impactMedium'
+  | 'impactHeavy'
+  | 'rigid'
+  | 'soft'
+  | 'notificationSuccess'
+  | 'notificationWarning'
+  | 'notificationError'
+  | 'clockTick'
+  | 'contextClick'
+  | 'keyboardPress'
+  | 'keyboardRelease'
+  | 'keyboardTap'
+  | 'longPress'
+  | 'textHandleMove'
+  | 'virtualKey'
+  | 'virtualKeyRelease'
+  | 'effectClick'
+  | 'effectDoubleClick'
+  | 'effectHeavyClick'
+  | 'effectTick';
+
+export const triggerHaptic = ReactNativeHapticFeedback
+  ? (method: HapticFeedbackTypes) => {
+      ReactNativeHapticFeedback.trigger(method, {
+        enableVibrateFallback: false,
+        ignoreAndroidSystemSettings: false,
+      });
+    }
+  : () => {};
