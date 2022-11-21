6f47977eae9d5b05f73dc8ec31d3882ddd0f0175
AWildmanPerchPeek <98963913+AWildmanPerchPeek@users.noreply.github.com>
2022-11-21 16:41:56 +0000

    Pending changes exported from your codespace

--- package/native-package/src/handlers/Sound.tsx
+++ package/native-package/src/handlers/Sound.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import AudioVideoPlayer from '../optionalDependencies/Video';
 
 export const Sound = {
@@ -5,22 +6,22 @@ export const Sound = {
   // eslint-disable-next-line react/display-name
   Player: AudioVideoPlayer
     ? ({ onBuffer, onEnd, onLoad, onProgress, paused, soundRef, style, uri }) => (
-        <AudioVideoPlayer
-          audioOnly={true}
-          onBuffer={onBuffer}
-          onEnd={onEnd}
-          onError={(error: Error) => {
-            console.log(error);
-          }}
-          onLoad={onLoad}
-          onProgress={onProgress}
-          paused={paused}
-          ref={soundRef}
-          source={{
-            uri,
-          }}
-          style={style}
-        />
-      )
+      <AudioVideoPlayer
+        audioOnly={true}
+        onBuffer={onBuffer}
+        onEnd={onEnd}
+        onError={(error: Error) => {
+          console.log(error);
+        }}
+        onLoad={onLoad}
+        onProgress={onProgress}
+        paused={paused}
+        ref={soundRef}
+        source={{
+          uri,
+        }}
+        style={style}
+      />
+    )
     : null,
 };
--- package/native-package/src/handlers/Video.tsx
+++ package/native-package/src/handlers/Video.tsx
@@ -1,22 +1,23 @@
+import React from 'react';
 import AudioVideoPlayer from '../optionalDependencies/Video';
 export const Video = AudioVideoPlayer
   ? ({ onBuffer, onEnd, onLoad, onProgress, paused, repeat, style, uri, videoRef }) => (
-      <AudioVideoPlayer
-        ignoreSilentSwitch={'ignore'}
-        onBuffer={onBuffer}
-        onEnd={onEnd}
-        onError={(error) => {
-          console.error(error);
-        }}
-        onLoad={onLoad}
-        onProgress={onProgress}
-        paused={paused}
-        ref={videoRef}
-        repeat={repeat}
-        source={{
-          uri,
-        }}
-        style={style}
-      />
-    )
+    <AudioVideoPlayer
+      ignoreSilentSwitch={'ignore'}
+      onBuffer={onBuffer}
+      onEnd={onEnd}
+      onError={(error) => {
+        console.error(error);
+      }}
+      onLoad={onLoad}
+      onProgress={onProgress}
+      paused={paused}
+      ref={videoRef}
+      repeat={repeat}
+      source={{
+        uri,
+      }}
+      style={style}
+    />
+  )
   : null;
