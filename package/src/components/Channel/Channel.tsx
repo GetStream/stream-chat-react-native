8b1eff9583ad0e6a6892f8037e3e40e0a972968c
Mads Røskar <mads.roskar@getstream.io>
2022-12-01 12:13:17 +0100

    feat: Make the DocumentPicker dependency optional (#1809)
    * feat: make the document picker optional
    
    * style: fix lint issues

--- package/src/components/Channel/Channel.tsx
+++ package/src/components/Channel/Channel.tsx
@@ -71,7 +71,7 @@ import {
   ThumbsUpReaction,
   WutReaction,
 } from '../../icons';
-import { FlatList as FlatListDefault } from '../../native';
+import { FlatList as FlatListDefault, pickDocument } from '../../native';
 import type { DefaultStreamChatGenerics } from '../../types/types';
 import { generateRandomId, MessageStatusTypes, ReactionData } from '../../utils/utils';
 import { Attachment as AttachmentDefault } from '../Attachment/Attachment';
@@ -447,7 +447,8 @@ const ChannelWithContext = <
     handleRetry,
     handleThreadReply,
     hasCommands = true,
-    hasFilePicker = true,
+    // If pickDocument isn't available, default to hiding the file picker
+    hasFilePicker = pickDocument !== null,
     hasImagePicker = true,
     hideDateSeparators = false,
     hideStickyDateHeader = false,
