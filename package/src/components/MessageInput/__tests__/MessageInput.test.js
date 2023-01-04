0c191264d1a7998448ef49410d06b73f8d164042
khushal87 <khushal.agarwal987@gmail.com>
2023-01-04 17:17:52 +0530

    tests: improve test coverage of MessageInput component

--- package/src/components/MessageInput/__tests__/MessageInput.test.js
+++ package/src/components/MessageInput/__tests__/MessageInput.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { Alert } from 'react-native';
+
 import { cleanup, fireEvent, render, waitFor } from '@testing-library/react-native';
 
+import * as AttachmentPickerUtils from '../../../contexts/attachmentPickerContext/AttachmentPickerContext';
 import { OverlayProvider } from '../../../contexts/overlayContext/OverlayProvider';
 import { getOrCreateChannelApi } from '../../../mock-builders/api/getOrCreateChannel';
 
 import { useMockedApis } from '../../../mock-builders/api/useMockedApis';
+import {
+  generateFileAttachment,
+  generateImageAttachment,
+} from '../../../mock-builders/generator/attachment';
 import { generateChannelResponse } from '../../../mock-builders/generator/channel';
 import { generateUser } from '../../../mock-builders/generator/user';
 import { getTestClientWithUser } from '../../../mock-builders/mock';
@@ -14,6 +21,36 @@ import { Chat } from '../../Chat/Chat';
 import { MessageInput } from '../MessageInput';
 
 describe('MessageInput', () => {
+  jest.spyOn(Alert, 'alert');
+  jest.spyOn(AttachmentPickerUtils, 'useAttachmentPickerContext').mockImplementation(
+    jest.fn(() => ({
+      closePicker: jest.fn(),
+      openPicker: jest.fn(),
+      selectedFiles: [
+        generateFileAttachment({ name: 'Dummy.png', size: 20000000 }),
+        generateFileAttachment({ name: 'Dummy.png', size: 20000000 }),
+      ],
+      selectedImages: [
+        generateImageAttachment({
+          file: { height: 100, uri: 'https://picsum.photos/200/300', width: 100 },
+          fileSize: 10000001,
+          uri: 'https://picsum.photos/200/300',
+        }),
+        generateImageAttachment({
+          file: { height: 100, uri: 'https://picsum.photos/200/300', width: 100 },
+          fileSize: 10000001,
+          uri: 'https://picsum.photos/200/300',
+        }),
+      ],
+      setBottomInset: jest.fn(),
+      setMaxNumberOfFiles: jest.fn(),
+      setSelectedFiles: jest.fn(),
+      setSelectedImages: jest.fn(),
+      setSelectedPicker: jest.fn(),
+      setTopInset: jest.fn(),
+    })),
+  );
+
   const clientUser = generateUser();
   let chatClient;
   let channel;
@@ -47,13 +84,8 @@ describe('MessageInput', () => {
 
   it('should render MessageInput', async () => {
     await initializeChannel(generateChannelResponse());
-    const openPicker = jest.fn();
-    const closePicker = jest.fn();
-    const attachmentValue = { closePicker, openPicker };
 
-    const { getByTestId, queryByTestId, queryByText, toJSON } = render(
-      getComponent({ attachmentValue }),
-    );
+    const { getByTestId, queryByTestId, queryByText, toJSON } = render(getComponent());
 
     await waitFor(() => {
       expect(queryByTestId('attach-button')).toBeTruthy();
@@ -62,9 +94,6 @@ describe('MessageInput', () => {
     fireEvent.press(getByTestId('attach-button'));
 
     await waitFor(() => {
-      expect(queryByTestId('upload-photo-touchable')).toBeTruthy();
-      expect(queryByTestId('upload-file-touchable')).toBeTruthy();
-      expect(queryByTestId('take-photo-touchable')).toBeTruthy();
       expect(queryByTestId('auto-complete-text-input')).toBeTruthy();
       expect(queryByTestId('send-button')).toBeTruthy();
       expect(queryByText('Editing Message')).toBeFalsy();
@@ -75,5 +104,26 @@ describe('MessageInput', () => {
     await waitFor(() => {
       expect(snapshot).toMatchSnapshot();
     });
+  });
+
+  it('trigger file size threshold limit alert when images size above the limit', async () => {
+    await initializeChannel(generateChannelResponse());
+
+    const initialProps = {
+      maxFileSizeToUploadInMb: 1,
+    };
+
+    render(
+      <OverlayProvider>
+        <Chat client={chatClient}>
+          <Channel channel={channel}>
+            <MessageInput {...initialProps} />
+          </Channel>
+        </Chat>
+      </OverlayProvider>,
+    );
+
+    // Both for files and for images triggered in one test itself.
+    expect(Alert.alert).toHaveBeenCalledTimes(2);
   });
 });
