0b32ee9fecbe4d128bde7062bd299d2045fe9d17
khushal87 <khushal.agarwal987@gmail.com>
2023-01-26 13:10:36 +0530

    fix: file upload issue in expo cli apps

--- package/src/components/AttachmentPicker/components/AttachmentPickerItem.tsx
+++ package/src/components/AttachmentPicker/components/AttachmentPickerItem.tsx
@@ -63,14 +63,10 @@ const AttachmentVideo: React.FC<AttachmentVideoProps> = (props) => {
 
   const size = vw(100) / (numberOfAttachmentPickerImageColumns || 3) - 2;
 
-  const onPressVideo = async () => {
+  const onPressVideo = () => {
     // For the case of expo messaging app where you need to fetch the asset uri from asset id
     if (selected) {
-      setSelectedFiles((files) =>
-        files.filter((file) => {
-          return file.uri !== asset.uri;
-        }),
-      );
+      setSelectedFiles((files) => files.filter((file) => file.uri !== asset.uri));
     } else {
       setSelectedFiles((files) => {
         if (numberOfUploads >= maxNumberOfFiles) {
