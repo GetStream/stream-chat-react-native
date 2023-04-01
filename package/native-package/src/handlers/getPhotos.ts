846e7afc6aebe06bb12216b5b2fe28923ca660f6
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-04-01 08:11:52 +0200

    fix: check for mediapermissions on rn 71 and android 33

--- package/native-package/src/handlers/getPhotos.ts
+++ package/native-package/src/handlers/getPhotos.ts
@@ -2,44 +2,59 @@ import { PermissionsAndroid, Platform } from 'react-native';
 
 import { CameraRoll, GetPhotosParams } from '@react-native-camera-roll/camera-roll';
 
+const verifyAndroidPermissions = async () => {
+  const isRN71orAbove = Platform.constants.reactNativeVersion?.minor >= 71;
+  const isAndroid13orAbove = Platform.Version >= 33;
+  const shouldCheckForMediaPermissions = isRN71orAbove && isAndroid13orAbove;
+  const getCheckPermissionPromise = () => {
+    if (shouldCheckForMediaPermissions) {
+      return Promise.all([
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
+      ]).then(
+        ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+          hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+      );
+    } else {
+      return PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+    }
+  };
+  const hasPermission = await getCheckPermissionPromise();
+  if (!hasPermission) {
+    const getRequestPermissionPromise = () => {
+      if (shouldCheckForMediaPermissions) {
+        return PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        ]).then(
+          (statuses) =>
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+              PermissionsAndroid.RESULTS.GRANTED,
+        );
+      } else {
+        return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, {
+          buttonNegative: 'Deny',
+          buttonNeutral: 'Ask Me Later',
+          buttonPositive: 'Allow',
+          message: 'Permissions are required to access and share photos.',
+          title: 'Photos Access',
+        }).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+      }
+    };
+    const granted = await getRequestPermissionPromise();
+    return granted;
+  }
+  return true;
+};
+
 export const getPhotos = async ({ after, first }: Pick<GetPhotosParams, 'after' | 'first'>) => {
   try {
     if (Platform.OS === 'android') {
-      const permissionCheckPromise =
-        Platform.Version >= 33
-          ? Promise.all([
-              PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
-              PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
-            ]).then(
-              ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
-                hasReadMediaImagesPermission && hasReadMediaVideoPermission,
-            )
-          : PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
-      const hasPermission = await permissionCheckPromise;
-      if (!hasPermission) {
-        const permissionRequestPromise =
-          Platform.Version >= 33
-            ? PermissionsAndroid.requestMultiple([
-                PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
-                PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
-              ]).then(
-                (statuses) =>
-                  statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
-                    PermissionsAndroid.RESULTS.GRANTED &&
-                  statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
-                    PermissionsAndroid.RESULTS.GRANTED,
-              )
-            : PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, {
-                buttonNegative: 'Deny',
-                buttonNeutral: 'Ask Me Later',
-                buttonPositive: 'Allow',
-                message: 'Permissions are required to access and share photos.',
-                title: 'Photos Access',
-              }).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
-        const granted = await permissionRequestPromise;
-        if (!granted) {
-          throw new Error('getPhotos Error');
-        }
+      const granted = await verifyAndroidPermissions();
+      if (!granted) {
+        throw new Error('getPhotos Error');
       }
     }
     const results = await CameraRoll.getPhotos({
