5cd2a8ba4c794110e46bc6156f03be7aa6f877d0
khushal87 <khushal.agarwal987@gmail.com>
2023-04-21 14:21:31 +0530

    fix: redundant condition for moving channel to top for hidden case

--- package/src/components/ChannelList/hooks/listeners/useNewMessage.ts
+++ package/src/components/ChannelList/hooks/listeners/useNewMessage.ts
@@ -44,17 +44,7 @@ export const useNewMessage = <
                 cid: event.cid,
               });
             }
-
-            // If channel doesn't exist in existing list, check in activeChannels as well.
-            // It may happen that channel was hidden using channel.hide(). In that case
-            // We remove it from `channels` state, but its still being watched and exists in client.activeChannels.
-            const channel = client.channel(event.channel_type, event.channel_id);
-
-            if (channel.initialized && channel.data?.hidden) {
-              return [channel, ...channels];
-            }
           }
-
           return [...channels];
         });
       }
