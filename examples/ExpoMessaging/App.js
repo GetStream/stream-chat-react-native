b8a9e8fa8220e8a73ecc3d1447a82c010d544d73
vanGalilea <galiliziv@gmail.com>
2023-04-24 15:06:55 +0200

    chore: upgrade expo

--- examples/ExpoMessaging/App.js
+++ examples/ExpoMessaging/App.js
@@ -157,7 +157,7 @@ const App = () => {
             translucentStatusBar
             value={{ style: theme }}
           >
-            <Chat client={chatClient} i18nInstance={streami18n} enableOfflineSupport>
+            <Chat client={chatClient} i18nInstance={streami18n} enableOfflineSupport={false}>
               {clientReady && (
                 <Stack.Navigator
                   initialRouteName='ChannelList'
