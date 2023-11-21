a9886bd9386bc47833e9203469a32c00e7d87b27
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-11-21 21:41:14 +0530

    fix: add comments for the change

--- package/src/components/Message/MessageSimple/utils/renderText.tsx
+++ package/src/components/Message/MessageSimple/utils/renderText.tsx
@@ -141,10 +141,12 @@ export const renderText = <
       : Linking.canOpenURL(url).then((canOpenUrl) => canOpenUrl && Linking.openURL(url));
   };
 
-  let previousLink: string;
+  let previousLink: string | undefined;
   const linkReact: ReactNodeOutput = (node, output, { ...state }) => {
     let url: string;
-    if (state?.withinLink) {
+    // Some long URLs with `&` separated parameters are trimmed and the url only until first param is taken.
+    // This is done because of internal link been taken from the original URL in react-native-markdown-package. So, we check for `withinLink` and take the previous full URL.
+    if (state?.withinLink && previousLink) {
       url = previousLink;
     } else {
       url = node.target;
