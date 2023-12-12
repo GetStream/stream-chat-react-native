799a0324ea39f508cee577b0abfd2255c08674a1
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-12-12 16:41:39 +0530

    fix: remove url encoding

--- package/src/components/Message/MessageSimple/utils/generateMarkdownText.ts
+++ package/src/components/Message/MessageSimple/utils/generateMarkdownText.ts
@@ -21,17 +21,16 @@ export const generateMarkdownText = (text?: string) => {
       length: 200,
       omission: '...',
     });
-    const decodedURL = decodeURI(linkInfo.encodedUrl);
     // Convert raw links/emails in the text to respective markdown syntax.
     // Eg: Hi @getstream.io -> Hi @[getstream.io](getstream.io).
     const normalRegEx = new RegExp(escapeRegExp(linkInfo.raw), 'g');
-    const markdown = `[${displayLink}](${decodedURL})`;
+    const markdown = `[${displayLink}](${linkInfo.url})`;
     resultText = text.replace(normalRegEx, markdown);
 
     // After previous step, in some cases, the mentioned user after `@` might have a link/email so we convert it back to normal raw text.
     // Eg: Hi, @[test.user@gmail.com](mailto:test.user@gmail.com) to @test.user@gmail.com.
     const mentionsRegex = new RegExp(
-      `@\\[${escapeRegExp(displayLink)}\\]\\(${escapeRegExp(decodedURL)}\\)`,
+      `@\\[${escapeRegExp(displayLink)}\\]\\(${escapeRegExp(linkInfo.url)}\\)`,
       'g',
     );
     resultText = resultText.replace(mentionsRegex, `@${displayLink}`);
