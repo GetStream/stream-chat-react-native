1922b10541eb808298cfee39b97636b9e5c2df37
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-02-28 10:30:41 +0100

    add exclusion list

--- examples/SampleApp/metro.config.js
+++ examples/SampleApp/metro.config.js
@@ -1,8 +1,10 @@
 /* eslint-env node */
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const { resolveUniqueModule } = require('@rnx-kit/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const { exclusionList, resolveUniqueModule } = require('@rnx-kit/metro-config');
 const PATH = require('path');
 const packageDir = PATH.resolve(__dirname, '../../package');
+
+const config = getDefaultConfig(__dirname);
 
 // find what all modules need to be unique for the app
 const dependencyPackageNames = Object.keys(require('./package.json').dependencies);
@@ -29,12 +31,8 @@ const extraNodeModules = uniqueModules.reduce((acc, item) => {
 
 extraNodeModules['stream-chat-react-native'] = PATH.resolve(packageDir, 'native-package');
 
-const customConfig = {
-  resolver: {
-    blockList,
-    extraNodeModules,
-  },
-  watchFolders,
-};
+config.resolver.blockList = exclusionList(blockList);
+config.resolver.extraNodeModules = extraNodeModules;
+config.watchFolders = [...config.watchFolders, ...watchFolders];
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), customConfig);
+module.exports = config;
