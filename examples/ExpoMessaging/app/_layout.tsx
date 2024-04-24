2a84f840252ff3cbc6b966e16b66b438fd224c1f
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-04-24 22:46:33 +0530

    fix: expo messaging app attachment picker safe area view

--- examples/ExpoMessaging/app/_layout.tsx
+++ examples/ExpoMessaging/app/_layout.tsx
@@ -1,23 +1,26 @@
-import { Stack } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { ChatWrapper } from "../components/ChatWrapper";
-import { AppProvider } from "../context/AppContext";
-import { StyleSheet } from "react-native";
+import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ChatWrapper } from '../components/ChatWrapper';
+import { AppProvider } from '../context/AppContext';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function Layout() {
-    return (
-        <GestureHandlerRootView style={styles.container}>
-            <ChatWrapper>
-                <AppProvider>
-                    <Stack />
-                </AppProvider>
-            </ChatWrapper>
-        </GestureHandlerRootView>
-    );
+  return (
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={styles.container}>
+        <ChatWrapper>
+          <AppProvider>
+            <Stack />
+          </AppProvider>
+        </ChatWrapper>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 });
