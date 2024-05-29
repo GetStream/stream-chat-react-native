772e2fda9f8eefb09ea856ff695a94692598e075
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-05-29 18:30:55 +0530

    feat: use queryReactions API to query and show reactions in OverlayReactions

---
+++ package/src/components/MessageOverlay/hooks/useFetchReactions.ts
@@ -0,0 +1,88 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+import { ReactionResponse, ReactionSort } from 'stream-chat';
+
+import { useChatContext } from '../../../contexts/chatContext/ChatContext';
+import { getReactionsForFilterSort } from '../../../store/apis/getReactionsforFilterSort';
+import { DefaultStreamChatGenerics } from '../../../types/types';
+
+export type UseFetchReactionParams<
+  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics,
+> = {
+  limit?: number;
+  messageId?: string;
+  reactionType?: string;
+  sort?: ReactionSort<StreamChatGenerics>;
+};
+
+export const useFetchReactions = <
+  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics,
+>({
+  limit = 25,
+  messageId,
+  reactionType,
+  sort,
+}: UseFetchReactionParams) => {
+  const [reactions, setReactions] = useState<ReactionResponse<StreamChatGenerics>[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+  const [next, setNext] = useState<string | undefined>(undefined);
+
+  const { client, enableOfflineSupport } = useChatContext();
+
+  const sortString = useMemo(() => JSON.stringify(sort), [sort]);
+
+  const loadOfflineReactions = () => {
+    if (!messageId) return;
+    const reactionsFromDB = getReactionsForFilterSort({
+      currentMessageId: messageId,
+      filters: reactionType ? { type: reactionType } : {},
+      sort,
+    });
+    if (reactionsFromDB) {
+      setReactions(reactionsFromDB);
+      setLoading(false);
+      setHasNextPage(false);
+    }
+  };
+
+  const loadOnlineReactions = async () => {
+    if (!messageId) return;
+    const response = await client.queryReactions(
+      messageId,
+      reactionType ? { type: reactionType } : {},
+      sort,
+      { limit, next },
+    );
+    if (response) {
+      setNext(response.next);
+      setHasNextPage(response.next !== undefined);
+      setReactions((prevReactions) => [...prevReactions, ...response.reactions]);
+      setLoading(false);
+    }
+  };
+
+  const fetchReactions = useCallback(async () => {
+    try {
+      if (enableOfflineSupport) {
+        loadOfflineReactions();
+      } else {
+        await loadOnlineReactions();
+      }
+    } catch (error) {
+      console.log('Error fetching reactions: ', error);
+    }
+  }, [client, messageId, reactionType, sortString, next]);
+
+  const loadNextPage = useCallback(async () => {
+    if (hasNextPage) {
+      await fetchReactions();
+    }
+  }, [hasNextPage]);
+
+  useEffect(() => {
+    fetchReactions();
+  }, [messageId, reactionType, sortString]);
+
+  return { loading, loadNextPage, reactions };
+};
