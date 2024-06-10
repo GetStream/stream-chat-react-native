970a3023126e3a02fe4aa41274c13c819ba45860
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-06-10 12:21:19 +0530

    fix: issues with expo-media-library in expo 51

--- examples/ExpoMessaging/components/AuthProgressLoader.tsx
+++ examples/ExpoMessaging/components/AuthProgressLoader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ActivityIndicator, StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const AuthProgressLoader = () => {
   return (
