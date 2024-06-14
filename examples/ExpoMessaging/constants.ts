859effc2aef85dd5b87dd623a4a082d30c69a976
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-06-14 16:13:22 +0530

    chore: change expo-media-library version

--- examples/ExpoMessaging/constants.ts
+++ examples/ExpoMessaging/constants.ts
@@ -1,6 +1,7 @@
-export const STREAM_API_KEY = 'q95x9hkbyd6p';
+export const STREAM_API_KEY = 'v3weqm7meazf';
 export const userToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9uIn0.eRVjxLvd4aqCEHY_JRa97g6k7WpHEhxL7Z4K4yTot1c';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidmlzaGFsIn0.7HqCXl_Sa8scpenH3A9pHpaBJNcxBD78gJDjcpAE4L0';
+
 export const user = {
-  id: 'ron',
+  id: 'vishal',
 };
