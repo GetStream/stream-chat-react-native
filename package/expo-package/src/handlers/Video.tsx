e8bc325ce3767ca5fa66543763569a7816c6730c
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-06-20 17:20:59 +0530

    fix: audio for the video muted when the silent mode is on for iOS

--- package/expo-package/src/handlers/Video.tsx
+++ package/expo-package/src/handlers/Video.tsx
@@ -1,16 +1,29 @@
-import { VideoComponent } from '../optionalDependencies';
+import React, { useEffect } from 'react';
+
+import { AudioComponent, VideoComponent } from '../optionalDependencies';
 
 export const Video = VideoComponent
-  ? ({ onPlaybackStatusUpdate, paused, resizeMode, style, uri, videoRef }) => (
-      <VideoComponent
-        onPlaybackStatusUpdate={onPlaybackStatusUpdate}
-        ref={videoRef}
-        resizeMode={resizeMode}
-        shouldPlay={!paused}
-        source={{
-          uri,
-        }}
-        style={[style]}
-      />
-    )
+  ? ({ onPlaybackStatusUpdate, paused, resizeMode, style, uri, videoRef }) => {
+      // This is done so that the audio of the video is not muted when the phone is in silent mode for iOS.
+      useEffect(() => {
+        const initializeSound = async () => {
+          await AudioComponent.setAudioModeAsync({
+            playsInSilentModeIOS: true,
+          });
+        };
+        initializeSound();
+      });
+      return (
+        <VideoComponent
+          onPlaybackStatusUpdate={onPlaybackStatusUpdate}
+          ref={videoRef}
+          resizeMode={resizeMode}
+          shouldPlay={!paused}
+          source={{
+            uri,
+          }}
+          style={[style]}
+        />
+      );
+    }
   : null;
