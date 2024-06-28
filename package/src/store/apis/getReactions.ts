047324c3e3a9acd7ec742f53bcc878c395bd2c03
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-06-28 12:52:17 +0530

    fix: issues with import

--- package/src/store/apis/getReactions.ts
+++ package/src/store/apis/getReactions.ts
@@ -2,7 +2,7 @@ import type { ReactionResponse } from 'stream-chat';
 
 import type { DefaultStreamChatGenerics } from '../../types/types';
 import { mapStorableToReaction } from '../mappers/mapStorableToReaction';
-import { QuickSqliteClient } from '../QuickSqliteClient';
+import { SqliteClient } from '../SqliteClient';
 import { TableRowJoinedUser } from '../types';
 
 export const getReactions = <
@@ -12,7 +12,7 @@ export const getReactions = <
 }: {
   reactions: TableRowJoinedUser<'reactions'>[];
 }): ReactionResponse<StreamChatGenerics>[] => {
-  QuickSqliteClient.logger?.('info', 'getReactions', { reactions });
+  SqliteClient.logger?.('info', 'getReactions', { reactions });
 
   // Enrich the channels with state
   return reactions.map((reaction) => ({
