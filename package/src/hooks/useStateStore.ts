01a77b32b48a5ceabb0a8d034a6ea730812a2ff6
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2024-10-28 16:25:11 +0100

    chore: bump stream-chat version in sdk

--- package/src/hooks/useStateStore.ts
+++ package/src/hooks/useStateStore.ts
@@ -4,15 +4,15 @@ import type { StateStore } from 'stream-chat';
 
 export function useStateStore<
   T extends Record<string, unknown>,
-  O extends Readonly<Record<string, unknown>>,
+  O extends Readonly<Record<string, unknown> | Readonly<unknown[]>>,
 >(store: StateStore<T>, selector: (v: T) => O): O;
 export function useStateStore<
   T extends Record<string, unknown>,
-  O extends Readonly<Record<string, unknown>>,
+  O extends Readonly<Record<string, unknown> | Readonly<unknown[]>>,
 >(store: StateStore<T> | undefined, selector: (v: T) => O): O | undefined;
 export function useStateStore<
   T extends Record<string, unknown>,
-  O extends Readonly<Record<string, unknown>>,
+  O extends Readonly<Record<string, unknown> | Readonly<unknown[]>>,
 >(store: StateStore<T> | undefined, selector: (v: T) => O) {
   const [state, setState] = useState<O | undefined>(() => {
     if (!store) return undefined;
