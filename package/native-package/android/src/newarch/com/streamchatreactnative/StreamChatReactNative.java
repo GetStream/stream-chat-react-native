8bc849e94c85850a14ebf90afee26e67958ea706
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-11-04 14:11:55 +0530

    fix: add StreamChatReactNative module for ios and android

--- package/native-package/android/src/newarch/com/streamchatreactnative/StreamChatReactNative.java
+++ package/native-package/android/src/newarch/com/streamchatreactnative/StreamChatReactNative.java
@@ -2,7 +2,7 @@ package com.streamchatreactnative;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 
-abstract class StreamChatReactNative extends NativeStreamChatReactNativeSpec {
+abstract class StreamChatReactNativeSpec extends NativeStreamChatReactNativeSpec {
   StreamChatReactNativeSpec(ReactApplicationContext context) {
     super(context);
   }
