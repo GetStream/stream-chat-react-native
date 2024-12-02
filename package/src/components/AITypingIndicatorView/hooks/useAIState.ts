fc9e275e6466f9ffdc7436063cb84022ddbea3a6
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2024-12-02 21:56:28 +0100

    fix: get rid of enum and introduce proper type

--- package/src/components/AITypingIndicatorView/hooks/useAIState.ts
+++ package/src/components/AITypingIndicatorView/hooks/useAIState.ts
@@ -4,13 +4,13 @@ import { Channel, Event } from 'stream-chat';
 
 import type { DefaultStreamChatGenerics } from '../../../types/types';
 
-export enum AIStatesEnum {
-  Error = 'AI_STATE_ERROR',
-  ExternalSources = 'AI_STATE_EXTERNAL_SOURCES',
-  Generating = 'AI_STATE_GENERATING',
-  Idle = 'AI_STATE_IDLE',
-  Thinking = 'AI_STATE_THINKING',
-}
+export type AIStateType =
+  | 'AI_STATE_ERROR'
+  | 'AI_STATE_EXTERNAL_SOURCES'
+  | 'AI_STATE_GENERATING'
+  | 'AI_STATE_IDLE'
+  | 'AI_STATE_THINKING'
+  | string;
 
 export const AIStates = {
   Error: 'AI_STATE_ERROR',
@@ -25,7 +25,7 @@ export const useAIState = <
 >(
   channel?: Channel<StreamChatGenerics>,
 ) => {
-  const [aiState, setAiState] = useState<AIStatesEnum>(AIStatesEnum.Idle);
+  const [aiState, setAiState] = useState<AIStateType>(AIStates.Idle);
 
   useEffect(() => {
     if (!channel) {
@@ -36,7 +36,7 @@ export const useAIState = <
       'ai_indicator_changed',
       (event: Event<StreamChatGenerics>) => {
         const { cid } = event;
-        const state = event.state as AIStatesEnum;
+        const state = event.state as AIStateType;
         if (channel.cid === cid) {
           setAiState(state);
         }
@@ -46,7 +46,7 @@ export const useAIState = <
     const indicatorClearedListener = channel.on('ai_indicator_clear', (event) => {
       const { cid } = event;
       if (channel.cid === cid) {
-        setAiState(AIStatesEnum.Idle);
+        setAiState(AIStates.Idle);
       }
     });
 
