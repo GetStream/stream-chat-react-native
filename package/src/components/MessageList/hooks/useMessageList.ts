09fc9b2790902d7625edba0f94501e327483b03a
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-01-10 22:07:39 +0530

    fix: read state for messages

--- package/src/components/MessageList/hooks/useMessageList.ts
+++ package/src/components/MessageList/hooks/useMessageList.ts
@@ -1,5 +1,7 @@
 import type { ChannelState, MessageResponse } from 'stream-chat';
 
+import { useLastReadData } from './useLastReadData';
+
 import {
   ChannelContextValue,
   useChannelContext,
@@ -14,7 +16,6 @@ import { useThreadContext } from '../../../contexts/threadContext/ThreadContext'
 import type { DefaultStreamChatGenerics } from '../../../types/types';
 import { getDateSeparators } from '../utils/getDateSeparators';
 import { getGroupStyles } from '../utils/getGroupStyles';
-import { getReadStates } from '../utils/getReadStates';
 
 export type UseMessageListParams = {
   deletedMessagesVisibilityType?: DeletedMessagesVisibilityType;
@@ -81,7 +82,11 @@ export const useMessageList = <
     userId: client.userID,
   });
 
-  const readData = getReadStates(client.userID, messageList, readList);
+  const readData = useLastReadData({
+    messages: messageList,
+    read: readList,
+    userID: client.userID,
+  });
 
   const messagesWithStylesReadByAndDateSeparator = messageList
     .filter((msg) => {
