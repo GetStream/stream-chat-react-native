ce7b74f632b12b81dd923a5a42895fcdc899a4ed
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-01-29 20:55:11 +0530

    fix: lint issues

--- package/src/components/ChannelPreview/ChannelPreviewMessenger.tsx
+++ package/src/components/ChannelPreview/ChannelPreviewMessenger.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
 import { ChannelAvatar } from './ChannelAvatar';
 import type { ChannelPreviewProps } from './ChannelPreview';
 import { ChannelPreviewMessage } from './ChannelPreviewMessage';
