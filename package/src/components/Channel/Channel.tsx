51893bb06cc50765c0f382e38c85d4a0010b06a9
Ivan Sekovanikj <31964049+isekovanic@users.noreply.github.com>
2025-02-19 10:02:41 +0100

    fix: state break on channel prop update (#2957)
    * fix: Channel and MessageList state breaking completely if the channel prop gets updated
    
    * fix: broken hook deps
    
    * chore: remove redundant log

--- package/src/components/Channel/Channel.tsx
+++ package/src/components/Channel/Channel.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useCallback, useEffect, useRef, useState } from 'react';
+import React, { PropsWithChildren, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { KeyboardAvoidingViewProps, StyleSheet, Text, View } from 'react-native';
 
 import debounce from 'lodash/debounce';
@@ -749,18 +749,20 @@ const ChannelWithContext = <
       ? newMessageStateUpdateThrottleInterval
       : stateUpdateThrottleInterval;
 
-  const copyChannelState = useRef(
-    throttle(
-      () => {
-        if (channel) {
-          copyStateFromChannel(channel);
-          copyMessagesStateFromChannel(channel);
-        }
-      },
-      copyChannelStateThrottlingTime,
-      throttleOptions,
-    ),
-  ).current;
+  const copyChannelState = useMemo(
+    () =>
+      throttle(
+        () => {
+          if (channel) {
+            copyStateFromChannel(channel);
+            copyMessagesStateFromChannel(channel);
+          }
+        },
+        copyChannelStateThrottlingTime,
+        throttleOptions,
+      ),
+    [channel, copyChannelStateThrottlingTime, copyMessagesStateFromChannel, copyStateFromChannel],
+  );
 
   const handleEvent: EventHandler<StreamChatGenerics> = (event) => {
     if (shouldSyncChannel) {
