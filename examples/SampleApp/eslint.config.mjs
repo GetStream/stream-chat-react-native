315273b96b25bfbd58fe4cc873c52e4d48b06ad2
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-02-21 16:00:34 +0100

    chore: bump eslint and all other dev deps

---
+++ examples/SampleApp/eslint.config.mjs
@@ -0,0 +1,43 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactNative from 'eslint-plugin-react-native';
+import eslintPluginComments from 'eslint-plugin-eslint-comments';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginJest from 'eslint-plugin-jest';
+
+import eslintReactNativeConfig from '@react-native/eslint-config';
+
+import typescriptParser from '@typescript-eslint/parser';
+
+const globals = Object.keys(eslintReactNativeConfig.globals).reduce((acc, key) => {
+  if (eslintReactNativeConfig.globals[key]) {
+    acc[key] = 'readonly';
+  }
+  return acc;
+}, {});
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/', '**/*.config.js'],
+  },
+  {
+    files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptParser,
+      globals,
+    },
+    plugins: {
+      react: eslintPluginReact,
+      'react-native': eslintPluginReactNative,
+      'eslint-comments': eslintPluginComments,
+      'react-hooks': eslintPluginReactHooks,
+      jest: eslintPluginJest,
+    },
+    rules: {
+      ...eslintReactNativeConfig.rules,
+      'jsx-quotes': ['error', 'prefer-single'],
+      'react-native/no-inline-styles': 'off',
+    },
+  },
+];
