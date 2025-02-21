b9cc1ace0c444762da61906a109fa3e8eb830dc7
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-02-21 23:54:46 +0100

    chore: upgrade typescript app to RN 0.77

--- examples/TypeScriptMessaging/ios/TypeScriptMessaging/AppDelegate.mm
+++ examples/TypeScriptMessaging/ios/TypeScriptMessaging/AppDelegate.mm
@@ -1,14 +1,16 @@
 #import "AppDelegate.h"
 #import <React/RCTBundleURLProvider.h>
+#import "RCTAppDependencyProvider.h"
 
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"TypeScriptMessaging";
+  self.dependencyProvider = [[RCTAppDependencyProvider alloc] init];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
