91e376abc1e99df4d940827d955651695719c96a
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-02-23 17:02:48 +0100

    fix: remove rule again

--- package/eslint.config.mjs
+++ package/eslint.config.mjs
@@ -89,7 +89,7 @@ export default tsEslint.config(
     rules: {
       ...reactNativeRules,
       'no-undef': 'off',
-      'prettier/prettier': 'warn',
+      // 'prettier/prettier': 'warn',
       'array-callback-return': 2,
       'arrow-body-style': 2,
       'comma-dangle': 0,
