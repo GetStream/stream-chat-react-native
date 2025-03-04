eec59501bfabf296247f3fac5536400797b5b484
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-03-04 23:05:45 +0100

    fix: take care of hoisting

--- package/native-package/src/optionalDependencies/pickDocument.ts
+++ package/native-package/src/optionalDependencies/pickDocument.ts
@@ -10,31 +10,41 @@ type ResponseValue = {
   uri: string;
 };
 
-let DocumentPicker:
+type DocumentPickerType =
   | {
       pick: (opts?: { allowMultiSelection: boolean; type: string[] }) => Promise<ResponseValue[]>;
       types: { allFiles: string };
     }
   | undefined;
 
+let DocumentPicker: DocumentPickerType;
+
+let OldDocumentPicker: DocumentPickerType;
+let NewDocumentPicker: DocumentPickerType;
+
 try {
-  DocumentPicker = require('@react-native-documents/picker');
+  NewDocumentPicker = require('@react-native-documents/picker');
 } catch (err) {
-  // we do nothing as the alternative might be used
-  // console.warn('@react-native-documents/picker is not installed');
+  // we log below
 }
 
-if (!DocumentPicker) {
-  try {
-    DocumentPicker = require('react-native-document-picker').default;
-    console.log(
-      "You're using the react-native-document-picker library, which is no longer supported and has moved to @react-native-documents/picker. Things might not work as intended. Please migrate to the new library as soon as possible !",
-    );
-  } catch (err) {
-    console.log(
-      'Neither react-native-document-picker nor @react-native-documents/picker are installed.',
-    );
-  }
+try {
+  OldDocumentPicker = require('react-native-document-picker').default;
+} catch (err) {
+  // we log below
+}
+
+if (NewDocumentPicker) {
+  DocumentPicker = NewDocumentPicker;
+} else if (OldDocumentPicker) {
+  DocumentPicker = OldDocumentPicker;
+  console.log(
+    "You're using the react-native-document-picker library, which is no longer supported and has moved to @react-native-documents/picker. Things might not work as intended. Please migrate to the new library as soon as possible !",
+  );
+} else {
+  console.log(
+    'Neither react-native-document-picker nor @react-native-documents/picker are installed.',
+  );
 }
 
 export const pickDocument = DocumentPicker
