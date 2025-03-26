a0f04e31a5dab6fcdb80e4391afbe7099b313757
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-03-26 20:04:18 +0100

    fix: remove await

--- package/src/components/Channel/Channel.tsx
+++ package/src/components/Channel/Channel.tsx
@@ -1484,7 +1484,7 @@ const ChannelWithContext = (props: PropsWithChildren<ChannelPropsWithContext>) =
     ];
 
     if (enableOfflineSupport) {
-      await addReactionToLocalState({
+      addReactionToLocalState({
         channel,
         enforceUniqueReaction,
         messageId,
