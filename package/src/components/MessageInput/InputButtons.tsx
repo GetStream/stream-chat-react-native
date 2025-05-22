3c3893cb9df9a00469ebe6e795644b9255cdf3c4
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-05-22 14:18:32 +0530

    fix: commands button hastext optimization

--- package/src/components/MessageInput/InputButtons.tsx
+++ package/src/components/MessageInput/InputButtons.tsx
@@ -83,7 +83,7 @@ export const InputButtonsWithContext = (props: InputButtonsWithContextProps) => 
           <AttachButton />
         </View>
       )}
-      {hasCommands && <CommandsButton />}
+      {hasCommands && <CommandsButton hasText={hasText} />}
     </>
   );
 };
