afabb1de39bf4a71862f6438f7250eb1dfb3a0b8
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-06-03 13:48:28 +0530

    tests: add tests

--- package/src/components/MessageInput/__tests__/AttachButton.test.js
+++ package/src/components/MessageInput/__tests__/AttachButton.test.js
@@ -1,31 +1,58 @@
 import React from 'react';
 
-import { render, screen, userEvent, waitFor } from '@testing-library/react-native';
+import { act, render, renderHook, screen, userEvent, waitFor } from '@testing-library/react-native';
 
-import { ThemeProvider } from '../../../contexts/themeContext/ThemeContext';
+import { OverlayProvider, useMessageInputContext } from '../../../contexts';
+
+import { initiateClientWithChannels } from '../../../mock-builders/api/initiateClientWithChannels';
+import * as NativeHandler from '../../../native';
+import { Channel } from '../../Channel/Channel';
+import { Chat } from '../../Chat/Chat';
 import { AttachButton } from '../AttachButton';
 
-describe('AttachButton', () => {
-  const getComponent = (props = {}) => (
-    <ThemeProvider>
-      <AttachButton {...props} />
-    </ThemeProvider>
+const renderComponent = ({ channelProps, client, props }) => {
+  return render(
+    <OverlayProvider>
+      <Chat client={client}>
+        <Channel {...channelProps}>
+          <AttachButton {...props} />
+        </Channel>
+      </Chat>
+    </OverlayProvider>,
   );
+};
 
-  it('should render an enabled AttachButton', async () => {
+describe('AttachButton', () => {
+  let client;
+  let channel;
+
+  beforeAll(async () => {
+    const { client: chatClient, channels } = await initiateClientWithChannels();
+    client = chatClient;
+    channel = channels[0];
+  });
+
+  it('should render an disabled AttachButton', async () => {
     const handleOnPress = jest.fn();
-    const user = userEvent.setup();
+    const channelProps = { channel };
+    const props = { disabled: true, handleOnPress };
 
-    render(getComponent({ handleOnPress }));
+    renderComponent({ channelProps, client, props });
+
+    const { queryByTestId } = screen;
 
     await waitFor(() => {
-      expect(screen.queryByTestId('attach-button')).toBeTruthy();
+      expect(queryByTestId('attach-button')).toBeTruthy();
       expect(handleOnPress).toHaveBeenCalledTimes(0);
     });
 
-    user.press(screen.getByTestId('attach-button'));
+    await act(() => {
+      userEvent.press(screen.getByTestId('attach-button'));
+    });
 
-    await waitFor(() => expect(handleOnPress).toHaveBeenCalledTimes(1));
+    await waitFor(() => {
+      expect(handleOnPress).toHaveBeenCalledTimes(0);
+    });
 
     const snapshot = screen.toJSON();
 
@@ -34,25 +61,107 @@ describe('AttachButton', () => {
     });
   });
 
-  it('should render a disabled AttachButton', async () => {
+  it('should render a enabled AttachButton', async () => {
     const handleOnPress = jest.fn();
-    const user = userEvent.setup();
+    const channelProps = { channel };
+    const props = { disabled: false, handleOnPress };
 
-    render(getComponent({ disabled: true, handleOnPress }));
+    renderComponent({ channelProps, client, props });
+
+    const { queryByTestId } = screen;
 
     await waitFor(() => {
-      expect(screen.queryByTestId('attach-button')).toBeTruthy();
+      expect(queryByTestId('attach-button')).toBeTruthy();
       expect(handleOnPress).toHaveBeenCalledTimes(0);
     });
 
-    user.press(screen.getByTestId('attach-button'));
+    await act(() => {
+      userEvent.press(screen.getByTestId('attach-button'));
+    });
 
-    await waitFor(() => expect(handleOnPress).toHaveBeenCalledTimes(0));
+    await waitFor(() => {
+      expect(handleOnPress).toHaveBeenCalledTimes(1);
+    });
 
     const snapshot = screen.toJSON();
 
     await waitFor(() => {
       expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  it('should call handleAttachButtonPress when the button is clicked if passed', async () => {
+    const handleAttachButtonPress = jest.fn();
+    const channelProps = { channel, handleAttachButtonPress };
+    const props = { disabled: false };
+
+    renderComponent({ channelProps, client, props });
+
+    const { queryByTestId } = screen;
+
+    await waitFor(() => {
+      expect(queryByTestId('attach-button')).toBeTruthy();
+      expect(handleAttachButtonPress).toHaveBeenCalledTimes(0);
+    });
+
+    await act(() => {
+      userEvent.press(screen.getByTestId('attach-button'));
+    });
+
+    await waitFor(() => {
+      expect(handleAttachButtonPress).toHaveBeenCalledTimes(1);
+    });
+
+    const snapshot = screen.toJSON();
+
+    await waitFor(() => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  it("should open native attachment picker when the media library isn't present", async () => {
+    jest.spyOn(NativeHandler, 'isImageMediaLibraryAvailable').mockImplementation(() => false);
+
+    const channelProps = { channel };
+    const props = {};
+
+    renderComponent({ channelProps, client, props });
+
+    const { queryByTestId } = screen;
+
+    await waitFor(() => {
+      expect(queryByTestId('attach-button')).toBeTruthy();
+    });
+
+    await act(() => {
+      userEvent.press(screen.getByTestId('attach-button'));
+    });
+
+    await waitFor(() => {
+      expect(queryByTestId('native-attachment-picker')).toBeTruthy();
+    });
+  });
+
+  it('should open stream attachment picker when the media library is present', async () => {
+    jest.spyOn(NativeHandler, 'isImageMediaLibraryAvailable').mockImplementation(() => true);
+
+    const channelProps = { channel };
+    const props = {};
+
+    renderComponent({ channelProps, client, props });
+
+    const { queryByTestId } = screen;
+
+    await waitFor(() => {
+      expect(queryByTestId('attach-button')).toBeTruthy();
+    });
+
+    await act(() => {
+      userEvent.press(screen.getByTestId('attach-button'));
+    });
+
+    await waitFor(() => {
+      expect(queryByTestId('attachment-picker-list')).toBeTruthy();
     });
   });
 });
