fe5d7663757fc5d859ff86bdbfc619085d6938fd
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-06-20 01:05:37 +0200

    fix: try removing RNFB references

--- examples/SampleApp/ios/AppDelegate.swift
+++ examples/SampleApp/ios/AppDelegate.swift
@@ -2,42 +2,34 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import FirebaseCore
-import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-  
+
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
-  
-  func application(
-      _ application: UIApplication,
-      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-      let delegate = ReactNativeDelegate()
-      let factory = RCTReactNativeFactory(delegate: delegate)
-      delegate.dependencyProvider = RCTAppDependencyProvider()
-      FirebaseApp.configure()
-   
-      reactNativeDelegate = delegate
-      reactNativeFactory = factory
-   
-      window = UIWindow(frame: UIScreen.main.bounds)
-   
-      factory.startReactNative(
-        withModuleName: "SampleApp",
-        in: window,
-        launchOptions: launchOptions
-      )
-   
-      return true
-    }
 
-  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      // Pass the device token to FCM
-      Messaging.messaging().apnsToken = deviceToken
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    factory.startReactNative(
+      withModuleName: "SampleApp",
+      in: window,
+      launchOptions: launchOptions
+    )
+
+    return true
   }
 }
 
