aa220747cd42b31c451315c580c5584a5d6a3c79
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-06-19 16:13:59 +0530

    fix: remove unused props from message input

--- package/src/components/MessageInput/MessageInput.tsx
+++ package/src/components/MessageInput/MessageInput.tsx
@@ -64,7 +64,6 @@ import {
   NativeHandlers,
 } from '../../native';
 import { AIStates, useAIState } from '../AITypingIndicatorView';
-import { AttachmentPickerProps } from '../AttachmentPicker/AttachmentPicker';
 import { AutoCompleteInput } from '../AutoCompleteInput/AutoCompleteInput';
 import { CreatePoll } from '../Poll/CreatePollContent';
 
@@ -106,20 +105,10 @@ const styles = StyleSheet.create({
   },
 });
 
-type MessageInputPropsWithContext = Partial<
-  Pick<
-    AttachmentPickerProps,
-    | 'AttachmentPickerError'
-    | 'AttachmentPickerErrorImage'
-    | 'AttachmentPickerIOSSelectMorePhotos'
-    | 'ImageOverlaySelectedComponent'
-    | 'attachmentPickerErrorButtonText'
-    | 'attachmentPickerErrorText'
-    | 'numberOfAttachmentImagesToLoadPerCall'
-    | 'numberOfAttachmentPickerImageColumns'
-  >
+type MessageInputPropsWithContext = Pick<
+  AttachmentPickerContextValue,
+  'bottomInset' | 'selectedPicker'
 > &
-  Pick<AttachmentPickerContextValue, 'bottomInset' | 'bottomSheetRef' | 'selectedPicker'> &
   Pick<ChatContextValue, 'isOnline'> &
   Pick<ChannelContextValue, 'channel' | 'members' | 'threadList' | 'watchers'> &
   Pick<
