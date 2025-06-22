48c30b26565b40fd0e0a4c27882e88db218af845
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-06-22 22:49:43 +0200

    fix: remove more deps

--- examples/SampleApp/src/utils/AsyncStore.ts
+++ examples/SampleApp/src/utils/AsyncStore.ts
@@ -1,9 +1,8 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default {
   getItem: async <T>(key: string, defaultValue: T | null) => {
-    const value = await AsyncStorage.getItem(key);
 
+    const value = null;
     if (!value) {
       return defaultValue;
     }
@@ -11,9 +10,7 @@ export default {
     return JSON.parse(value) as T;
   },
   removeItem: async (key: string) => {
-    await AsyncStorage.removeItem(key);
   },
   setItem: async <T>(key: string, value: T) => {
-    await AsyncStorage.setItem(key, JSON.stringify(value));
   },
 };
