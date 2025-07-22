6a76c1fdf8d244824b982574e5a68cbee255fa30
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-07-22 12:13:28 +0530

    fix: add translations for live locations and reminders

--- examples/ExpoMessaging/components/ChatWrapper.tsx
+++ examples/ExpoMessaging/components/ChatWrapper.tsx
@@ -26,10 +26,6 @@ export const ChatWrapper = ({ children }: PropsWithChildren<{}>) => {
     tokenOrProvider: userToken,
   });
 
-  streami18n.registerTranslation('en', {
-    ...enTranslations,
-    'timestamp/Location end at': '{{ milliseconds | durationFormatter(withSuffix: false) }}',
-  });
   const theme = useStreamChatTheme();
 
   if (!chatClient) {
