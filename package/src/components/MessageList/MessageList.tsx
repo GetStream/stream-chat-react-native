3bd3e1d67a502280eda67938f0de46e989ff3b38
Dave Gomez <davegomez426@gmail.com>
2025-08-04 15:21:19 -0300

    Fix the scroll-to-bottom issue when deleting the last sent message

--- package/src/components/MessageList/MessageList.tsx
+++ package/src/components/MessageList/MessageList.tsx
@@ -594,7 +594,7 @@ const MessageListWithContext = (props: MessageListPropsWithContext) => {
       if (
         topMessageBeforeUpdate.current?.created_at &&
         topMessageAfterUpdate?.created_at &&
-        topMessageBeforeUpdate.current.created_at < topMessageAfterUpdate.created_at
+        topMessageBeforeUpdate.current.created_at <= topMessageAfterUpdate.created_at
       ) {
         channelResyncScrollSet.current = false;
         setScrollToBottomButtonVisible(false);
