84630c3f45296db9ac016689c463ec736647209d
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-08-26 14:13:20 +0530

    fix: remove extra properties

--- package/src/contexts/themeContext/utils/theme.ts
+++ package/src/contexts/themeContext/utils/theme.ts
@@ -1520,7 +1520,6 @@ export const defaultTheme: Theme = {
       container: {},
     },
     targetedMessageContainer: {},
-    unreadUnderlayColor: undefined,
     videoThumbnail: {
       container: {},
       roundedView: {},
