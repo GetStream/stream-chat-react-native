ea084aa3fce3657cb5d1eb4dfd2fa921b4cacd42
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-10-13 14:42:25 +0530

    feat: add video playing optional capability through expo-av (#3248)

--- package/expo-package/src/optionalDependencies/AudioVideo.ts
+++ package/expo-package/src/optionalDependencies/AudioVideo.ts
@@ -1,17 +1,20 @@
 let AudioComponent;
+let VideoComponent;
 let RecordingObject;
+
 try {
   const audioVideoPackage = require('expo-av');
   AudioComponent = audioVideoPackage.Audio;
+  VideoComponent = audioVideoPackage.Video;
   RecordingObject = audioVideoPackage.RecordingObject;
 } catch (e) {
   // do nothing
 }
 
-if (!AudioComponent) {
+if (!AudioComponent || !VideoComponent) {
   console.log(
     'Audio Video library is currently not installed. To allow in-app audio playback, install the "expo-av" package.',
   );
 }
 
-export { AudioComponent, RecordingObject };
+export { AudioComponent, RecordingObject, VideoComponent };
