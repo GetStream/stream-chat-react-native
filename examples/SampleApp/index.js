d03c9053687fb7db654a2e9721dca8d1505e689f
Khushal Agarwal <khushal.agarwal987@gmail.com>
2025-10-22 13:27:44 +0530

    feat: add delivery receipt support inside push notification

--- examples/SampleApp/index.js
+++ examples/SampleApp/index.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import { enableScreens } from 'react-native-screens';
+import './src/utils/bootstrapBackgroundMessageHandler';
 
 import App from './App';
 import { name as appName } from './app.json';
