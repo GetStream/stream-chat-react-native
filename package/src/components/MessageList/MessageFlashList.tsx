d409eb8f17ab3a55666de03d54ca402242f20e96
Ivan Sekovanikj <31964049+isekovanic@users.noreply.github.com>
2025-11-28 11:33:05 +0100

    feat: add support for ephemeral channels (#3301)
    ## 🎯 Goal
    
    This PR provides a variety of fixes in our Chat SDK that made it either
    impossible (or too difficult to do without hacks) for us to send a
    message in an ephemeral channel. At a certain point, this should be
    abstracted away in a more sophisticated fashion however it'll have to do
    for now.
    
    It also adds 2 new properties on the `Channel` component that can be
    used as utilities for this usecase.
    
    ## 🛠 Implementation details
    
    <!-- Provide a description of the implementation -->
    
    ## 🎨 UI Changes
    
    <!-- Add relevant screenshots -->
    
    <details>
    <summary>iOS</summary>
    
    
    <table>
        <thead>
            <tr>
                <td>Before</td>
                <td>After</td>
            </tr>
        </thead>
        <tbody>
            <tr>
                <td>
                    <!--<img src="" /> -->
                </td>
                <td>
                    <!--<img src="" /> -->
                </td>
            </tr>
        </tbody>
    </table>
    </details>
    
    
    <details>
    <summary>Android</summary>
    
    <table>
        <thead>
            <tr>
                <td>Before</td>
                <td>After</td>
            </tr>
        </thead>
        <tbody>
            <tr>
                <td>
                    <!--<img src="" /> -->
                </td>
                <td>
                    <!--<img src="" /> -->
                </td>
            </tr>
        </tbody>
    </table>
    </details>
    
    ## 🧪 Testing
    
    <!-- Explain how this change can be tested (or why it can't be tested)
    -->
    
    ## ☑️ Checklist
    
    - [ ] I have signed the [Stream
    CLA](https://docs.google.com/forms/d/e/1FAIpQLScFKsKkAJI7mhCr7K9rEIOpqIDThrWxuvxnwUq2XkHyG154vQ/viewform)
    (required)
    - [ ] PR targets the `develop` branch
    - [ ] Documentation is updated
    - [ ] New code is tested in main example apps, including all possible
    scenarios
      - [ ] SampleApp iOS and Android
      - [ ] Expo iOS and Android

--- package/src/components/MessageList/MessageFlashList.tsx
+++ package/src/components/MessageList/MessageFlashList.tsx
@@ -718,7 +718,7 @@ const MessageFlashListWithContext = (props: MessageFlashListPropsWithContext) =>
 
   const renderItem = useCallback(
     ({ index, item: message }: { index: number; item: LocalMessage }) => {
-      if (!channel || channel.disconnected || (!channel.initialized && !channel.offlineMode)) {
+      if (!channel || channel.disconnected) {
         return null;
       }
 
