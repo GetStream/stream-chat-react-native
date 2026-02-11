357c46100d2b98089db329e2d3e59a08a40d1d7e
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-02-11 16:37:59 +0100

    chore: remove unused cb

--- package/src/components/AttachmentPicker/AttachmentPicker.tsx
+++ package/src/components/AttachmentPicker/AttachmentPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import {
   BackHandler,
   EmitterSubscription,
