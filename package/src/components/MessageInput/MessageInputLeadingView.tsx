0dec251296ba3d0742aed5f749fdfc7976dba760
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-02-26 00:03:01 +0100

    fix: edge cases with with commands picker

--- package/src/components/MessageInput/MessageInputLeadingView.tsx
+++ package/src/components/MessageInput/MessageInputLeadingView.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+
+import { AttachmentManagerState } from 'stream-chat';
 
 import { textComposerStateSelector } from './utils/messageComposerSelectors';
 
@@ -8,12 +10,27 @@ import { useStateStore } from '../../hooks/useStateStore';
 import { primitives } from '../../theme';
 import { GiphyChip } from '../ui/GiphyChip';
 
+const hasAttachmentsSelector = (nextState: AttachmentManagerState) => ({
+  hasAttachments: nextState.attachments?.length > 0,
+});
+
 export const MessageInputLeadingView = () => {
   const messageComposer = useMessageComposer();
   const { textComposer } = messageComposer;
+  // TODO: V9: This needs to come from the LLC.
+  const { hasAttachments } = useStateStore(
+    messageComposer.attachmentManager.state,
+    hasAttachmentsSelector,
+  );
   const { command } = useStateStore(textComposer.state, textComposerStateSelector);
 
-  return command ? (
+  useEffect(() => {
+    if (hasAttachments) {
+      textComposer.clearCommand();
+    }
+  }, [textComposer, hasAttachments]);
+
+  return command && !hasAttachments ? (
     <View style={styles.giphyContainer}>
       <GiphyChip />
     </View>
