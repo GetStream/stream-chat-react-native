0536ba67500db4940bdc232bf08fa5a9f6553b67
Khushal Agarwal <khushal.agarwal987@gmail.com>
2026-03-03 12:24:33 +0530

    fix: refactor icons in the SDK

--- package/src/icons/Lightning.tsx
+++ package/src/icons/Lightning.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
-import Svg, { Path } from 'react-native-svg';
+import { Path, Svg } from 'react-native-svg';
 
 import { IconProps } from './utils/base';
 
-type Props = IconProps & {
-  size: number;
-};
-
-export const Lightning = ({ size = 32, ...rest }: Props) => (
-  <Svg height={size} viewBox={`0 0 ${size} ${size}`} width={size} {...rest}>
+export const Lightning = ({ height, width, ...rest }: IconProps) => (
+  <Svg fill={'none'} height={height} viewBox={'0 0 12 12'} width={width} {...rest}>
     <Path
-      d='M14.8522 28H13.5188L14.8522 18.6667H10.1855C9.01218 18.6667 9.74551 17.6667 9.77218 17.6267C11.4922 14.5867 14.0788 10.0533 17.5322 4H18.8655L17.5322 13.3333H22.2122C22.7455 13.3333 23.0388 13.5867 22.7455 14.2133C17.4788 23.4 14.8522 28 14.8522 28Z'
+      d='M8.2266 1.16648C8.524 0.279984 7.4007 -0.417073 6.7514 0.291481L1.73231 5.76855C1.21953 6.32815 1.60932 7.24205 2.37725 7.24205H4.79225C4.87431 7.24205 4.93956 7.3243 4.91215 7.41115L3.82717 10.8489C3.54697 11.7367 4.67246 12.4136 5.3123 11.7034L10.2732 6.1964C10.7789 5.6351 10.3876 4.72846 9.6232 4.72846H7.20825C7.1253 4.72846 7.0598 4.64444 7.0891 4.5571L8.2266 1.16648Z'
       {...rest}
     />
   </Svg>
