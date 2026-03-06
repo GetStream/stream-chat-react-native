e952c46b8ac3b3f69280c60f4c91cbdcdf31c67f
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-03-06 15:40:16 +0100

    refactor: solve rn compatibility issues and use shared codebase for expo sdk

---
+++ package/expo-package/react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        packageImportPath: 'import com.streamchatexpo.StreamChatExpoPackage;',
+        packageInstance: 'new StreamChatExpoPackage()',
+      },
+    },
+  },
+};
