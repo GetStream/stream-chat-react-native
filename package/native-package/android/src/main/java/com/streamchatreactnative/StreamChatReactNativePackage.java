9e8b6620ed8cae774b45b86e06726cfb77a3f69c
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-03-06 17:21:20 +0100

    fix: shared files packaging

--- package/native-package/android/src/main/java/com/streamchatreactnative/StreamChatReactNativePackage.java
+++ package/native-package/android/src/main/java/com/streamchatreactnative/StreamChatReactNativePackage.java
@@ -8,7 +8,6 @@ import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.TurboReactPackage;
 import com.facebook.react.uimanager.ViewManager;
 
-
 import java.util.HashMap;
 import java.util.Collections;
 import java.util.List;
