2139d896043ba4182906e5e0352dc7589bfbe3ae
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-03-07 18:17:38 +0100

    chore: always use native shimmer view

--- package/src/components/Attachment/ImageLoadingIndicator.tsx
+++ package/src/components/Attachment/ImageLoadingIndicator.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
-import { ActivityIndicator, Platform, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 
 import { useTheme } from '../../contexts';
 import { NativeShimmerView } from '../UIComponents/NativeShimmerView';
-import { ShimmerView } from '../UIComponents/Shimmer/ShimmerView';
 
 export const ImageLoadingIndicator = () => {
   const {
     theme: { semantics },
   } = useTheme();
-  if (Platform.OS === 'android' || Platform.OS === 'ios') {
-    return (
-      <NativeShimmerView
-        enabled
-        gradientColor={semantics.skeletonLoadingHighlight}
-        style={StyleSheet.absoluteFillObject}
-      >
-        <View pointerEvents='none' style={styles.centered}>
-          <ActivityIndicator />
-        </View>
-      </NativeShimmerView>
-    );
-  }
-
   return (
-    <View style={StyleSheet.absoluteFillObject}>
-      <ShimmerView>
+    <NativeShimmerView
+      enabled
+      gradientColor={semantics.skeletonLoadingHighlight}
+      style={StyleSheet.absoluteFillObject}
+    >
+      <View pointerEvents='none' style={styles.centered}>
         <ActivityIndicator />
-      </ShimmerView>
-    </View>
+      </View>
+    </NativeShimmerView>
   );
 };
 
