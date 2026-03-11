f175d271b4da626c1414c3880a33bd6af1e68c3a
Khushal Agarwal <khushal.agarwal987@gmail.com>
2026-03-11 15:23:23 +0530

    fix: remove deprecated useSyncDatabase hook

--- package/src/components/Chat/hooks/index.ts
+++ package/src/components/Chat/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useCreateChatClient';
 export * from './useIsOnline';
-export * from './useSyncDatabase';
 export * from './useAppSettings';
 export * from './useClientMutedUsers';
 export * from './useCreateChatContext';
