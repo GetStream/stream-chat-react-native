f379a86eb6529c972ab9a5e38a16711dac7c6eab
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-04-08 10:26:45 +0200

    fix: unicode replacement string android crash

--- package/src/components/Message/MessageItemView/utils/generateMarkdownText.test.ts
+++ package/src/components/Message/MessageItemView/utils/generateMarkdownText.test.ts
@@ -4,6 +4,9 @@ describe('generateMarkdownText', () => {
   it.each([
     ['', null],
     ['  test message  ', 'test message'],
+    ['\uFFFC', null],
+    [' \uFFFC ', null],
+    ['hello\uFFFCworld', 'hello world'],
     ['https://www.getstream.io', '[https://www.getstream.io](https://www.getstream.io)'],
     [
       'https://getstream-production.s3-accelerate.amazonaws.com/N336903591601695/33e78ef89e64642862a75c5cca2541eaf6b1c924/trimmedVideos/alert/2_270_881/outputVideo.mp4?AWSAccessKeyId=AKIAVJAW2AD2SQVQCBXV&Expires=1699998768&Signature=zdEMCGzf4Pq++16YkPprvN5NAds=',
