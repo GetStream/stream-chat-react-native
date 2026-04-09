b17b412565c77f4ed2e0ed4f04f2eb0f780276ea
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-04-09 13:07:36 +0200

    fix: gap for long titles

--- package/src/components/ChannelPreview/ChannelPreviewView.tsx
+++ package/src/components/ChannelPreview/ChannelPreviewView.tsx
@@ -204,6 +204,7 @@ const useStyles = () => {
       },
       contentContainer: { flex: 1, gap: primitives.spacingXxs },
       upperRow: {
+        gap: primitives.spacingMd,
         alignItems: 'center',
         flex: 1,
         flexDirection: 'row',
