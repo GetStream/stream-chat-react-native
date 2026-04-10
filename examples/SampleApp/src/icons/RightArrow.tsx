364e6f5ccacf59471c3411086acd50541d0e6f0c
Zita Szupera <szuperaz@gmail.com>
2026-04-10 10:25:46 +0200

    fix: mirror icons in RTL mode

--- examples/SampleApp/src/icons/RightArrow.tsx
+++ examples/SampleApp/src/icons/RightArrow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { I18nManager } from 'react-native';
 import { G, Path, Svg } from 'react-native-svg';
 
 import { IconProps } from '../utils/base';
@@ -17,14 +18,16 @@ export const RightArrow = ({
 
   return (
     <Svg fill='none' height={height ?? size} viewBox='0 0 20 20' width={width ?? size} {...rest}>
-      <G transform='matrix(-1 0 0 1 20 0)'>
-        <Path
-          d='M16.875 10H3.125M3.125 10L8.75 4.375M3.125 10L8.75 15.625'
-          stroke={color}
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth={1.5}
-        />
+      <G transform={I18nManager.isRTL ? 'matrix(-1 0 0 1 20 0)' : undefined}>
+        <G transform='matrix(-1 0 0 1 20 0)'>
+          <Path
+            d='M16.875 10H3.125M3.125 10L8.75 4.375M3.125 10L8.75 15.625'
+            stroke={color}
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={1.5}
+          />
+        </G>
       </G>
     </Svg>
   );
