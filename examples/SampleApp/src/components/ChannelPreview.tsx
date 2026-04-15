df10cd56aa0ef5c91031ee688bd91a2cb366aaa1
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-04-15 02:46:20 +0200

    fix: lint issues

--- examples/SampleApp/src/components/ChannelPreview.tsx
+++ examples/SampleApp/src/components/ChannelPreview.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import {
-  ChannelPreviewView,
-  ChannelPreviewViewProps,
   ChannelPreviewStatus,
   ChannelPreviewStatusProps,
   Pin,
