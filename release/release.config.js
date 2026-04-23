b5c8b990745bb8e3b2552b8d25392f374a74d291
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-04-23 13:23:00 +0200

    chore: remove dryRun

--- release/release.config.js
+++ release/release.config.js
@@ -82,7 +82,7 @@ module.exports = Promise.resolve().then(() => {
   const lernaPackage = require('../lerna.json');
 
   return {
-    dryRun: true,
+    dryRun: false,
     extends: [`${__dirname}/monorepo-setup.js`],
     workspaces: lernaPackage.packages,
     filterPath: process.env.FILTER_PATH,
