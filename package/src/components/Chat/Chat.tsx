2419df8d153b52f56c633d9f501c8d136e4c7bfe
Ivan Sekovanikj <31964049+isekovanic@users.noreply.github.com>
2026-04-30 16:39:15 +0200

    fix: revert SDK name update (#3581)
    Reverts GetStream/stream-chat-react-native#3576
    
    This has since been updated on the backend, so it's no longer relevant
    or necessary.

--- package/src/components/Chat/Chat.tsx
+++ package/src/components/Chat/Chat.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren, useEffect, useMemo, useState } from 'react';
 import { Platform } from 'react-native';
 
-import { Channel, OfflineDBState, SdkIdentifier } from 'stream-chat';
+import { Channel, OfflineDBState } from 'stream-chat';
 
 import { useClientMutedUsers } from './hooks';
 import { useAppSettings } from './hooks/useAppSettings';
@@ -187,10 +187,9 @@ const ChatWithContext = (props: PropsWithChildren<ChatProps>) => {
 
   useEffect(() => {
     if (client) {
-      const sdkName = (((NativeHandlers.SDK
-        ? NativeHandlers.SDK.replace('stream-chat-', '')
-        : 'react-native') as 'react-native' | 'expo') +
-        `-${Platform.OS as 'ios' | 'android'}`) as SdkIdentifier['name'];
+      const sdkName = (
+        NativeHandlers.SDK ? NativeHandlers.SDK.replace('stream-chat-', '') : 'react-native'
+      ) as 'react-native' | 'expo';
       client.sdkIdentifier = {
         name: sdkName,
         version,
