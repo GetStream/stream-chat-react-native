9134886f04c9a16e37eab3bd06d5d9992608e200
Zita Szupera <szuperaz@gmail.com>
2026-05-26 11:42:06 -0500

    fix: lint

--- package/src/hooks/__tests__/useUserActions.test.tsx
+++ package/src/hooks/__tests__/useUserActions.test.tsx
@@ -1,10 +1,10 @@
 import { renderHook, waitFor } from '@testing-library/react-native';
 import type { UserResponse } from 'stream-chat';
 
+import * as useNotificationApiModule from '../../components/Notifications/hooks/useNotificationApi';
 import * as ChatContext from '../../contexts/chatContext/ChatContext';
 import type { TranslationContextValue } from '../../contexts/translationContext/TranslationContext';
 import * as TranslationContext from '../../contexts/translationContext/TranslationContext';
-import * as useNotificationApiModule from '../../components/Notifications/hooks/useNotificationApi';
 import { useUserActions } from '../useUserActions';
 
 const createUser = (overrides?: Partial<UserResponse>): UserResponse =>
