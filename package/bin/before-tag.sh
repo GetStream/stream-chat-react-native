1667235dc504fa24c1d0040861628a5fd28be0fb
Ivan Sekovanikj <31964049+isekovanic@users.noreply.github.com>
2026-05-27 18:04:22 +0200

    fix: switch to `npm` actions with `--no-workspaces` (#3622)
    ## 🎯 Goal
    
    This PR should address the release process issues we introduced with the
    Yarn Berry implementation.
    
    ## 🛠 Implementation details
    
    <!-- Provide a description of the implementation -->
    
    ## 🎨 UI Changes
    
    <!-- Add relevant screenshots -->
    
    <details>
    <summary>iOS</summary>
    
    
    <table>
        <thead>
            <tr>
                <td>Before</td>
                <td>After</td>
            </tr>
        </thead>
        <tbody>
            <tr>
                <td>
                    <!--<img src="" /> -->
                </td>
                <td>
                    <!--<img src="" /> -->
                </td>
            </tr>
        </tbody>
    </table>
    </details>
    
    
    <details>
    <summary>Android</summary>
    
    <table>
        <thead>
            <tr>
                <td>Before</td>
                <td>After</td>
            </tr>
        </thead>
        <tbody>
            <tr>
                <td>
                    <!--<img src="" /> -->
                </td>
                <td>
                    <!--<img src="" /> -->
                </td>
            </tr>
        </tbody>
    </table>
    </details>
    
    ## 🧪 Testing
    
    <!-- Explain how this change can be tested (or why it can't be tested)
    -->
    
    ## ☑️ Checklist
    
    - [ ] I have signed the [Stream
    CLA](https://docs.google.com/forms/d/e/1FAIpQLScFKsKkAJI7mhCr7K9rEIOpqIDThrWxuvxnwUq2XkHyG154vQ/viewform)
    (required)
    - [ ] PR targets the `develop` branch
    - [ ] Documentation is updated
    - [ ] New code is tested in main example apps, including all possible
    scenarios
      - [ ] SampleApp iOS and Android
      - [ ] Expo iOS and Android

--- package/bin/before-tag.sh
+++ package/bin/before-tag.sh
@@ -11,12 +11,12 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 PACKAGE_VERSION_WITHOUT_SHA=$(echo "$PACKAGE_VERSION" | cut -d"+" -f1)
 
 cd native-package
-npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION_WITHOUT_SHA"
+npm version --no-workspaces --allow-same-version --no-git-tag-version "$PACKAGE_VERSION_WITHOUT_SHA"
 sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION_WITHOUT_SHA"'"|g' -i.bak package.json
 rm package.json.bak
 
 cd ../expo-package
-npm version --allow-same-version --no-git-tag-version "$PACKAGE_VERSION_WITHOUT_SHA"
+npm version --no-workspaces --allow-same-version --no-git-tag-version "$PACKAGE_VERSION_WITHOUT_SHA"
 sed -e 's|"stream-chat-react-native-core": "[^"]*"|"stream-chat-react-native-core": "'"$PACKAGE_VERSION_WITHOUT_SHA"'"|g' -i.bak package.json
 rm package.json.bak
 cd ..
@@ -26,6 +26,3 @@ rm src/version.json.bak
 
 git add {expo,native}-package/package.json
 git add src/version.json
-
-git add expo-package/yarn.lock
-git add native-package/yarn.lock
