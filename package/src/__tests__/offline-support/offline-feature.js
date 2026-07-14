375f06ed3d18254d405848d2e67dd330367a944c
Ivan Sekovanikj <31964049+isekovanic@users.noreply.github.com>
2026-07-14 16:16:01 +0200

    fix: thread_manager reload before activation (#3729)
    ## 🎯 Goal
    
    Backport of [this
    PR](https://github.com/GetStream/stream-chat-react-native/pull/3720) to
    V8.
    
    ## 🛠 Implementation details
    
    <!-- Provide a description of the implementation -->
    
    ## 🎨 UI Changes
    
    <!-- Add relevant screenshots -->
    
    <details>
    <summary>iOS</summary>
    
    
    <table>
        <thead>
            <tr>
                <td>Before</td>
                <td>After</td>
            </tr>
        </thead>
        <tbody>
            <tr>
                <td>
                    <!--<img src="" /> -->
                </td>
                <td>
                    <!--<img src="" /> -->
                </td>
            </tr>
        </tbody>
    </table>
    </details>
    
    
    <details>
    <summary>Android</summary>
    
    <table>
        <thead>
            <tr>
                <td>Before</td>
                <td>After</td>
            </tr>
        </thead>
        <tbody>
            <tr>
                <td>
                    <!--<img src="" /> -->
                </td>
                <td>
                    <!--<img src="" /> -->
                </td>
            </tr>
        </tbody>
    </table>
    </details>
    
    ## 🧪 Testing
    
    <!-- Explain how this change can be tested (or why it can't be tested)
    -->
    
    ## ☑️ Checklist
    
    - [ ] I have signed the [Stream
    CLA](https://docs.google.com/forms/d/e/1FAIpQLScFKsKkAJI7mhCr7K9rEIOpqIDThrWxuvxnwUq2XkHyG154vQ/viewform)
    (required)
    - [ ] PR targets the `develop` branch
    - [ ] Documentation is updated
    - [ ] New code is tested in main example apps, including all possible
    scenarios
      - [ ] SampleApp iOS and Android
      - [ ] Expo iOS and Android

--- package/src/__tests__/offline-support/offline-feature.js
+++ package/src/__tests__/offline-support/offline-feature.js
@@ -1438,22 +1438,16 @@ export const Generic = () => {
       await waitFor(() => expect(screen.getByTestId('channel-list')).toBeTruthy());
       const targetChannel = channels[getRandomInt(0, channels.length - 1)];
 
-      const oldMemberCount = targetChannel.channel.member_count;
       const newMember = generateMember();
       act(() => dispatchMemberAddedEvent(chatClient, newMember, targetChannel.channel));
 
       await waitFor(async () => {
         const membersRows = await BetterSqlite.selectFromTable('members');
-        const channelRows = await BetterSqlite.selectFromTable('channels');
         const matchingMembersRows = membersRows.filter(
           (m) => m.cid === targetChannel.channel.cid && m.userId === newMember.user_id,
         );
-        const targetChannelFromDb = channelRows.filter(
-          (c) => c.cid === targetChannel.channel.cid,
-        )[0];
 
         expect(matchingMembersRows.length).toBe(1);
-        expect(targetChannelFromDb.memberCount).toBe(oldMemberCount + 1);
       });
     });
 
@@ -1467,21 +1461,15 @@ export const Generic = () => {
 
       const targetChannel = channels[getRandomInt(0, channels.length - 1)];
       const targetMember = targetChannel.members[getRandomInt(0, targetChannel.members.length - 1)];
-      const oldMemberCount = targetChannel.channel.member_count;
       act(() => dispatchMemberRemovedEvent(chatClient, targetMember, targetChannel.channel));
 
       await waitFor(async () => {
         const membersRows = await BetterSqlite.selectFromTable('members');
-        const channelRows = await BetterSqlite.selectFromTable('channels');
         const matchingMembersRows = membersRows.filter(
           (m) => m.cid === targetChannel.channel.cid && m.userId === targetMember.user_id,
         );
-        const targetChannelFromDb = channelRows.filter(
-          (c) => c.cid === targetChannel.channel.cid,
-        )[0];
 
         expect(matchingMembersRows.length).toBe(0);
-        expect(targetChannelFromDb.memberCount).toBe(oldMemberCount - 1);
       });
     });
 
