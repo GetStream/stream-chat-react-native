1d14b5f87020300197635ddb36f2e89926bd7260
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2026-09-24 11:49:47 +0200

    fix: revert message item view changes

--- package/src/components/Message/MessageItemView/MessageItemView.tsx
+++ package/src/components/Message/MessageItemView/MessageItemView.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 
 import { SwipableMessageWrapper } from './MessageBubble';
 
@@ -128,7 +128,7 @@ export type MessageItemViewPropsWithContext = Pick<
   >;
 
 const MessageItemViewWithContext = (props: MessageItemViewPropsWithContext) => {
-  const { width } = useWindowDimensions();
+  const { width } = Dimensions.get('window');
   const {
     alignment,
     channel,
